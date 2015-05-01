571bbbd3997903ea73dfe4a20d71ff7895588dff
Roman Yurchak <rth@crans.org>
2015-05-01 18:24:04 +0200

    Optimized the basis set generation and added tests

--- BASEX/tests/test_basis_set.py
+++ BASEX/tests/test_basis_set.py
@@ -5,7 +5,8 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 
-from BASEX.core import get_left_right, parse_matlab
+from BASEX.core import get_left_right
+from BASEX.io import parse_matlab
 from BASEX.basis import generate_basis
 
 DATA_DIR = os.path.join(os.path.split(__file__)[0], '../data/')
@@ -17,7 +18,7 @@ def setup():
 
 def test_consistency_included_dataset():
     # just a sanity check
-    path = os.path.join(DATA_DIR, 'basex_basis_1000x1000.npy')
+    path = os.path.join(DATA_DIR, 'basex_basis_1000_orig.npy')
 
     left, right, M, Mc = np.load(path)
 
@@ -29,10 +30,6 @@ def test_consistency_included_dataset():
 
     #Ni, Nj = M.shape
     ##rawdata = np.random.randn(Ni, Ni).view(np.matrix)
-    #print(left.shape)
-    #print(right.shape)
-    #print(M.shape)
-    #print(Mc.shape)
 
     #Ci = (left*rawdata)*right
     #P = (Mc*Ci)*M.T
@@ -40,16 +37,25 @@ def test_consistency_included_dataset():
     #print(rawdata)
     #yield assert_allclose, rawdata, P
 
-def test_basis_generation():
+def test_generation_basis40():
     base_dir = os.path.join(DATA_DIR, 'ascii')
-    M_ref, Mc_ref = parse_matlab('basis40', base_dir)
+    for size in [40, 100]:
+        M_ref, Mc_ref = parse_matlab('basis{}'.format(size), base_dir, gzip=False)
 
-    M, Mc = generate_basis(41, 20)
+        M, Mc = generate_basis(size+1, size//2)
 
-    yield assert_allclose, Mc_ref.view(np.ndarray), Mc
-    yield assert_allclose, M_ref.view(np.ndarray), M
+        yield assert_allclose, Mc_ref.view(np.ndarray), Mc
+        yield assert_allclose, M_ref.view(np.ndarray), M
 
 
+#def test_generation_basis100():
+#    left, right, M_ref, Mc_ref = np.load(os.path.join(DATA_DIR, 'basex_basis_1000x1000.npy'))
+#
+#    M, Mc = generate_basis(101, 50, verbose=True)
+#
+#
+#    yield assert_allclose, Mc_ref.view(np.ndarray), Mc.view(np.ndarray)
+#    yield assert_allclose, M_ref.view(np.ndarray), M.view(np.ndarray)
 
 
 
