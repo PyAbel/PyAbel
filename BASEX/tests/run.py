7a4473e6938c0d1adcaa45c64e21d2eba694c7ad
Roman Yurchak <rth@crans.org>
2015-11-17 11:01:04 +0100

    Issue #8: added a more robust mechanism for running tests

---
+++ BASEX/tests/run.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+_base_dir, _ = os.path.split(__file__)
+
+def run(coverage=False):
+    import nose
+    argv=['', '-s', '--where={}'.format(_base_dir), '--verbosity=2']
+    if coverage:
+        argv += ['--with-coverage', '--cover-package=BASEX']
+    result = nose.run(argv=argv)
+    status = int(not result)
+    return status
+
+def run_cli(coverage=False):
+    status = run(coverage=coverage)
+    print('Exit status: {}'.format(status))
+    sys.exit(status)
+
+
+
+if __name__ == '__main__':
+    run_cli()
+
+
+
+
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='PyAbel',
       version='0.5.0',
       description='A Python package for inverse Abel transforms',
       author='Dan Hickstein',
-      packages=['abel'],
+      packages=find_packages(),
       package_data={'abel': ['abel/data/*','abel/tests/data/*' ]},
       #test_suite="BASEX.tests.run"
      )
