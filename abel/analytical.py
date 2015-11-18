c950ff5990a7b62124ae9e4b7b78e9e0014ba176
Roman Yurchak <rth@crans.org>
2015-11-18 10:37:01 +0100

    Issue #8: cleaned up symetric step example

--- abel/analytical.py
+++ abel/analytical.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+
+class SymStep(object):
+    def __init__(self, n, r_max, r1, r2, A0=1.0):
+        """
+        Define a a symmetric step function and calculate it's analytical
+        Abel transform. See examples/example_step.py
+
+        Parameters
+           - n : int: number of points along the r axis
+           - r_max: float: range of the symmetric r interval
+           - r1, r2: floats: bounds of the step function if r > 0
+                    ( symetic function is constructed for r < 0)
+           - A0: float: height of the step
+        """
+
+        self.n = n
+        self.r_max = r_max
+        self.r1, self.r2 = r1, r2
+        self.A0 = A0
+
+        self.r = r = np.linspace(-r_max, r_max, n)
+        mask = np.abs(np.abs(r)- 0.5*(r1 + r2)) < 0.5*(r2 - r1)
+        self.dr =  np.diff(r)[0]
+        fr = np.zeros(r.shape)
+        fr[mask] = A0
+        self.func = fr
+
+    @property
+    def abel(self):
+        """ Return the direct Abel transform """
+        return sym_abel_step_1d(self.r, self.A0, self.r1, self.r2)[0]
+
+
 def abel_step_analytical(r, A0, r0, r1):
     """
     Direct Abel transform of a step function located between r0 and r1,
