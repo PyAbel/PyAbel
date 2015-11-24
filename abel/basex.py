34713a832022ddc56f1151acbec7e0d47c736d1b
DhrubajyotiDas <ddas328@gmail.com>
2015-11-24 14:54:32 -0500

    Added asymmetric basis set generation functions

--- abel/basex.py
+++ abel/basex.py
@@ -200,6 +200,22 @@ def _get_left_right_matrices(M, Mc):
     M_right = M.dot(inv((M.T.dot(M) + E)))
     return M_left, M_right
 
+def _get_left_right_matrices_asymmetric(M_vert, M_horz, Mc_vert, Mc_horz): 
+    """ An internal helper function for no-up/down-asymmetry BASEX:
+
+        given basis sets  M_vert, M_horz, Mc_vert, Mc_horz,
+        return M_left and M_right matrices 
+    """
+
+    nbf_vert, nbf_horz = np.shape(M_vert)[1], np.shape(M_horz)[1]
+    
+    q_vert, q_horz = 0,0
+    E_vert, E_horz = np.identity(nbf_vert)*q_vert, np.identity(nbf_horz)*q_horz
+
+    vert_left = inv(Mc_vert.T.dot(Mc_vert) + E_vert).dot(Mc_vert.T)
+    horz_right = M_horz.dot(inv(M_horz.T.dot(M_horz) + E_horz))
+
+    return vert_left, horz_right
 
 def _nbf_default(n, nbf):
     """ An internal helper function to check that nbf = n//2 and print a waring
@@ -272,6 +288,63 @@ def get_basis_sets_cached(n, nbf='auto', basis_dir='.', verbose=False):
                 print('Basis set saved for later use to,')
                 print(' '*10 + '{}'.format(path_to_basis_file))
     return M, Mc, M_left, M_right
+
+
+def get_basis_sets_cached_asymmetric(n, nbf='auto', basis_dir='.', verbose=False):
+    """
+    Internal function.
+
+    Get basis sets, using the disk as a cache 
+    (i.e. load from disk if they exist, if not calculate them and save a copy on disk).
+
+    Parameters:
+    -----------
+      - n : odd integer: Abel inverse transform will be performed on a `n x n`
+        area of the image
+      - nbf: integer: number of basis functions. If nbf='auto', it is set to (n-1)/2.
+      - basis_dir : path to the directory for saving / loading the basis set coefficients.
+                    If None, the basis sets will not be saved to disk. 
+    """
+
+    nbf = _nbf_default(n, nbf)
+
+    # SOMETHING FOR NBF_VERT HERE
+
+    basis_name = "basex_asymm_basis_{}_{}_{}_{}.npy".format(n_vert, n_horz, nbf_vert, nbf_horz)
+
+    M_horz = None
+    if basis_dir is not None:
+        path_to_basis_file = os.path.join(basis_dir, basis_name)
+        if os.path.exists(path_to_basis_file): # Use existing basis set
+            if verbose:
+                print('Loading basis sets...           ')
+                # saved as a .npy file
+            try:
+                M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right, M_version  = np.load(path_to_basis_file)
+            except ValueError:
+                raise
+
+    if M_horz is None: # generate the basis set
+        if verbose:
+            print('A suitable basis set was not found.',
+                  'A new basis set will be generated.',
+                  'This may take a few minutes. ', end='')
+            if basis_dir is not None:
+                print('But don\'t worry, it will be saved to disk for future use.\n')
+            else:
+                print(' ')
+
+        M_vert, M_horz, Mc_vert, Mc_horz  = generate_basis_sets_asymmetric(n_vert, n_horz, nbf_vert, nbf_horz, verbose=verbose)
+        vert_left, horz_right = _get_left_right_matrices_asymmetric(M_vert, M_horz, Mc_vert, Mc_horz)
+
+        if basis_dir is not None:
+            np.save(path_to_basis_file,
+                    (M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right,  np.array(__version__)))
+            if verbose:
+                print('Basis set saved for later use to,')
+                print(' '*10 + '{}'.format(path_to_basis_file))
+
+    return M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right
 
 
 MAX_BASIS_SET_OFFSET = 4000
@@ -381,3 +454,143 @@ def generate_basis_sets(n, nbf='auto', verbose=False):
         print("...{}".format(k+1))
 
     return M, Mc
+
+def generate_basis_sets_asymmetric(n_vert=1001, n_horz = 501, 
+                                    nbf_vert = 1001, nbf_horz = 251, 
+                                    verbose=True):    
+    """ 
+    Generates basis sets for the BASEX method without assuming up/down symmetry. 
+
+    Parameters:
+    -----------
+      n_vert : integer : Vertical dimensions of the image in pixels. 
+      n_horz : integer : Horizontal dimensions of the image in pixels. Must be odd.
+      nbf_vert : integer : Number of basis functions in the z-direction. Must be less than or equal (default) to n_vert 
+      nbf_horz: integer: Number of basis functions in the x-direction. Must be less than or equal (default) to n_horz//2 + 1
+
+    Returns:
+    --------
+      M_vert, M_horz, Mc_vert, Mc_horz : numpy arrays
+    """
+    if n_horz % 2 == 0:
+        raise ValueError('The horizontal dimensions of the image (n_horz) must be odd.')
+
+    if (n_horz+1)//2 < nbf_horz:
+        raise ValueError('The number of horizontal basis functions (nbf_horz) cannot be greater than n_horz//2 + 1')
+
+    if n_vert < nbf_vert:
+        raise ValueError('The number of vertical basis functions (nbf_vert) cannot be greater than the number of vertical pixels (n_vert).')
+
+    Rm_h = n_horz//2 + 1
+
+    I_h = np.arange(1, n_horz + 1)
+
+    R2_h = (I_h - Rm_h)**2
+    M_horz = np.zeros((n_horz, nbf_horz))
+    Mc_horz = np.zeros((n_horz, nbf_horz))
+
+    M_horz[:,0] = 2*np.exp(-R2_h)
+    Mc_horz[:,0] = np.exp(-R2_h)
+
+    gammaln_0o5 = gammaln(0.5) 
+
+    if verbose:
+        print('Generating horizontal BASEX basis sets for n_horz = {}, nbf_horz = {}:\n'.format(n_horz, nbf_horz))
+        sys.stdout.write('0')
+        sys.stdout.flush()
+
+    # the number of elements used to calculate the projected coefficeints
+    delta = np.fmax(np.arange(nbf_horz)*32 - MAX_BASIS_SET_OFFSET, MAX_BASIS_SET_OFFSET) 
+    for k in range(1, nbf_horz):
+        k2 = k*k # so we don't recalculate it all the time
+        log_k2 = log(k2) 
+        angn = exp(
+                    k2 * (1 - log_k2) + 
+                    gammaln(k2 + 0.5) - gammaln_0o5  
+                    # old form --> k2 - 2 * k2*log(k) +
+                    #              np.log(np.arange(0.5, k2 + 0.5)).sum()
+                    )
+        M_horz[Rm_h-1, k] =  2 * angn
+
+        for l in range(1, n_horz - Rm_h + 1):
+            l2 = l*l
+            log_l2 = log(l2)
+
+            val = exp(k2 * (1 + log(l2/k2)) - l2) 
+            # old form --> val = exp(k2 - l2 + 2 * k2*log((1.0 * l)/k)) 
+
+            Mc_horz[Rm_h - 1 + l, k] = val # All rows below center
+            Mc_horz[Rm_h - 1 - l, k] = val # All rows above center
+
+            aux = val + angn * Mc_horz[Rm_h - 1 + l, 0]
+
+            p = np.arange(max(1, l2 - delta[k]), min(k2 - 1,  l2 + delta[k]) + 1)
+
+            # We use here the fact that for p, k real and positive
+            #
+            #  np.log(np.arange(p, k)).sum() == gammaln(k) - gammaln(p) 
+            #
+            # where gammaln is scipy.misc.gammaln (i.e. the log of the Gamma function)
+            #
+            # The following line corresponds to the vectorized third
+            # loop of the original BASIS2.m matlab file.
+
+            aux += np.exp(k2 - l2 - k2*log_k2 + p*log_l2
+                      + gammaln(k2 + 1) - gammaln(p + 1) 
+                      + gammaln(k2 - p + 0.5) - gammaln_0o5
+                      - gammaln(k2 - p + 1)
+                      ).sum()
+
+            # End of vectorized third loop
+
+            aux *= 2
+
+            M_horz[Rm_h - 1 + l, k] = aux # All rows below center
+            M_horz[Rm_h - 1 - l, k] = aux # All rows above center
+
+        if verbose and k % 50 == 0:
+            sys.stdout.write('...{}'.format(k))
+            sys.stdout.flush()
+
+    if verbose:
+        print("...{}".format(k+1))
+
+    ####################################    
+    # Axial functions
+    ####################################
+
+    Z2_h = np.arange(0, n_vert)**2
+
+    M_vert = np.zeros((n_vert, nbf_vert))
+    Mc_vert = np.zeros((n_vert, nbf_vert))
+
+    # M_vert[:,0] = 2*np.exp(-Z2_h)
+    Mc_vert[:,0] = np.exp(-Z2_h)
+
+    if verbose:
+        print('Generating vertical BASEX basis sets for n_vert = {}, nbf_vert = {}:\n'.format(n_vert, nbf_vert))
+        sys.stdout.write('0')
+        sys.stdout.flush()
+
+    # delta_v = np.fmax(np.arange(nbf_vert)*32 - 0, 8000) 
+
+    for k in range(1, nbf_vert):
+        k2 = k*k
+        log_k2 = log(k2)
+
+        for l in range(1, n_vert):
+            l2 = l*l
+            log_l2 = log(l2)
+
+            val = exp(k2 * (1 + log(l2/k2)) - l2) 
+
+            Mc_vert[l, k] = val 
+
+        if verbose and k % 50 == 0:
+            sys.stdout.write('...{}'.format(k))
+            sys.stdout.flush()
+
+    if verbose:
+        print("...{}".format(k+1))
+
+    return M_vert, M_horz, Mc_vert, Mc_horz 
