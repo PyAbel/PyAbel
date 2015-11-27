e42216bb0a2462ef8866f335a26282299c38c587
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2015-11-27 16:49:07 +1100

    Hansen and Law: updated code according to @DanHickstein pull request 1 conversation, including parallel processing, tuple quandrant selection, single reform image block. Example reduced image size 1024x, changed to O2- as more strcuture. Renamed functions according to issue 10 (consistent naming scheme) - I think!

--- abel/hansenlaw.py
+++ abel/hansenlaw.py
@@ -5,13 +5,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import numpy as np
+import multiprocessing as mp 
 from time import time
 from math import exp, log, pow, pi
 from abel.tools import calculate_speeds
 
 ######################################################################
-# HasenLaw - an alternative inverse Abel transformation algorithm 
-#            to basex
+# hasenlaw - an alternative inverse Abel transformation algorithm 
 #
 # Stephen Gibson - Australian National University, Australia
 # Jason Gascooke - Flinders University, Australia
@@ -19,7 +19,7 @@ from abel.tools import calculate_speeds
 # This algorithm is adapted by Jason Gascooke from the article
 #   E. W. Hansen and P-L. Law
 #  "Recursive methods for computing the Abel transform and its inverse"
-#  J. Opt. Soc. Am A2, 510-520 (1985)
+#   J. Opt. Soc. Am A2, 510-520 (1985) doi: 10.1364/JOSAA.2.000510
 #
 # J. R. Gascooke PhD Thesis:
 #  "Energy Transfer in Polyatomic-Rare Gas Collisions and Van Der Waals 
@@ -31,34 +31,41 @@ from abel.tools import calculate_speeds
 #
 ########################################################################
 
-def HansenLaw_transform (ImgRow):
+def iabel_hansenlaw_transform (ImgRow):
     """ Hansen and Law J. Opt. Soc. Am A2, 510-520 (1985)
-        much simpler than BASEX!
+         Eqs. (17) & (18)
     """
     h   = [0.318,0.19,0.35,0.82,1.8,3.9,8.3,19.6,48.3]
     lam = [0.0,-2.1,-6.2,-22.4,-92.5,-414.5,-1889.4,-8990.9,-47391.1]
+
     Gamma = lambda Nm, lam: (1.0-pow(Nm,lam))/(pi*lam)\
-            if lam < -1 else -np.log(Nm)/pi
+            if lam < -1 else -np.log(Nm)/pi         # Eq. (18)
+
     K = np.size(h);  N = np.size(ImgRow)
     X = np.zeros(K); AImgRow = np.zeros(N)
 
-    gp = np.gradient (ImgRow)
-    for n in range(0,N-1):
+    gp = np.gradient (ImgRow)   # derivative
+    for n in range(N-1):        # each pixel of row
         Nm = (N-n)/(N-n-1.0)
-        for k in range(0,K):
-            X[k] = pow(Nm,lam[k])*X[k] + h[k]*Gamma(Nm,lam[k])*gp[n]
+        for k in range(K):
+            X[k] = pow(Nm,lam[k])*X[k] + h[k]*Gamma(Nm,lam[k])*gp[n] # Eq. (17)
         AImgRow[n] = X.sum()
 
     AImgRow[N-1] = AImgRow[N-2]  # special case for N=N-1
     return -AImgRow
 
-def HansenLaw (data,mask=0b1111,calc_speeds=True,verbose=True):
+
+def iabel_hansenlaw (data,quad=(True,True,True,True),calc_speeds=True,verbose=True,freecpus=1):
     """ Split image into quadrants and co-add
-        mask =   Q3 Q2 Q1 Q0
+
+        quad =   Q0 Q1 Q2 Q3
+
                 ( Q1 | Q0)
                  --------
                 ( Q2 | Q3)
     """  
+    pool = mp.Pool(processes=mp.cpu_count()-freecpus) 
+
     (N,M)=np.shape(data)
     N2 = N//2
     if verbose:
@@ -74,40 +81,38 @@ def HansenLaw (data,mask=0b1111,calc_speeds=True,verbose=True):
     Q3 = np.fliplr(np.flipud(Q3))
 
 # combine selected quadrants into one or loop through if none 
-    if mask>0:
-        if verbose:
-            print ("HL: co-adding quadrants mask={:s}".format(bin(mask)))
+    if np.any(quad):
+        if verbose: print ("HL: Co-adding quadrants")
 
-        Q = Q0*(mask&(1<<0))+Q1*(mask&(1<<1))+Q2*(mask&(1<<2))+Q3*(mask&(1<<3))
+        Q = Q0*quad[0]+Q1*quad[1]+Q2*quad[2]+Q3*quad[3]
 
+        if verbose: print ("HL: Calculating inverse Abel transform ... ",end='')
         # inverse Abel transform of combined quadrant, applied to each row
-        AQ=[HansenLaw_transform(Q[row]) for row in range(N2)]
 
-        # reform image
-        Top    = np.concatenate ((AQ,np.fliplr(AQ)),axis=1)
-        Bottom = np.flipud(Top)
-        recon  = np.concatenate ((Top,Bottom),axis=0)
+        AQ0 = pool.map(iabel_hansenlaw_transform,[Q[row] for row in range(N2)])
+
+        AQ3 = AQ2 = AQ1 = AQ0  # all quadrants the same
 
     else:
-        if verbose: print ("HL: individual quadrants")
+        if verbose: print ("HL: Individual quadrants")
 
-        # Fix me! - there should be a more python way
-        # inversion each quandrante, one row at a time
-        AQ0 = [HansenLaw_transform(Q0[row]) for row in range(N2)]
-        AQ1 = [HansenLaw_transform(Q1[row]) for row in range(N2)]
-        AQ2 = [HansenLaw_transform(Q2[row]) for row in range(N2)]
-        AQ3 = [HansenLaw_transform(Q3[row]) for row in range(N2)]
+        # inversion of each quandrant, one row at a time
+        if verbose: print ("HL: Calculating inverse Abel transform ... ",end='')
+        AQ0 = pool.map(iabel_hansenlaw_transform,[Q0[row] for row in range(N2)])
+        AQ1 = pool.map(iabel_hansenlaw_transform,[Q1[row] for row in range(N2)])
+        AQ2 = pool.map(iabel_hansenlaw_transform,[Q2[row] for row in range(N2)])
+        AQ3 = pool.map(iabel_hansenlaw_transform,[Q3[row] for row in range(N2)])
 
-        # reform image
-        Top    = np.concatenate ((AQ1,np.fliplr(AQ0)),axis=1)
-        Bottom = np.flipud(np.concatenate ((AQ2,np.fliplr(AQ3)),axis=1))
-        recon  = np.concatenate ((Top,Bottom),axis=0)
+    # reform image
+    Top    = np.concatenate ((AQ1,np.fliplr(AQ0)),axis=1)
+    Bottom = np.flipud(np.concatenate ((AQ2,np.fliplr(AQ3)),axis=1))
+    recon  = np.concatenate ((Top,Bottom),axis=0)
             
-    if verbose:
-        print ("HL: Inverse Abel transform in {:.2f} seconds".format(time()-t0))
+    if verbose: print ("{:.2f} seconds".format(time()-t0))
+
     if calc_speeds:
         if verbose:
-            print('Generating speed distribution...')
+            print('Generating speed distribution ...',end='')
             t1 = time()
 
         speeds = calculate_speeds(recon, N)
