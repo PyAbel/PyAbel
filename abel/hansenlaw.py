5b04e49ca49b9c3686795edbd94ebedb97cf09a3
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2015-11-29 13:39:32 +1100

    update HL function documentation. Clone example_main.py for example_hansenlaw.py, for consistent look

--- abel/hansenlaw.py
+++ abel/hansenlaw.py
@@ -1,4 +1,3 @@
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -10,7 +9,7 @@ from time import time
 from math import exp, log, pow, pi
 from abel.tools import calculate_speeds
 
-######################################################################
+###########################################################################
 # hasenlaw - an alternative inverse Abel transformation algorithm 
 #
 # Stephen Gibson - Australian National University, Australia
@@ -29,11 +28,28 @@ from abel.tools import calculate_speeds
 #
 # Note: the inversion is carried out one image row at a time
 #
-########################################################################
+###########################################################################
 
 def iabel_hansenlaw_transform (ImgRow):
-    """ Hansen and Law J. Opt. Soc. Am A2, 510-520 (1985)
+    """ Inverse Abel transformation using the algorithm of: 
+        Hansen and Law J. Opt. Soc. Am A2, 510-520 (1985).
          Eqs. (17) & (18)
+
+        Parameters:
+        ----------
+         - ImgRow: a N/2 numpy vector 
+           |       one row from am image, orientated top/left
+           |     +--------+              --------+ 
+           \=>   |      * |               *      |
+                 |   *    |                  *   |
+                 |  *     |                   *  |
+                 +--------+              --------+
+                               |  *     |     *  |
+                               |   *    |    *   |
+                               |     *  | *      |
+                               +--------+--------+
+
+        recursive inversion starts at the leftside outer edge
     """
     h   = [0.318,0.19,0.35,0.82,1.8,3.9,8.3,19.6,48.3]
     lam = [0.0,-2.1,-6.2,-22.4,-92.5,-414.5,-1889.4,-8990.9,-47391.1]
@@ -56,13 +72,45 @@ def iabel_hansenlaw_transform (ImgRow):
 
 
 def iabel_hansenlaw (data,quad=(True,True,True,True),calc_speeds=True,verbose=True,freecpus=1):
-    """ Split image into quadrants and co-add
+    """ Split image into quadrants and co-add,
+        Hansen Law inverse Abel transform,
+        reassemble,
+        and (optionally) calculate the radial integration of the image (calc_speeds)
 
-        quad =   Q0 Q1 Q2 Q3
+        Parameters:
+        ----------
+         - data: a NxN numpy array
+         - quad:  boolean tuple, (Q0,Q1,Q2,Q3)
+                 +--------+--------+                
+                 | Q1   * | *   Q0 |
+                 |   *    |    *   |  
+                 |  *     |     *  |
+                 +--------+--------+
+                 |  *     |     *  |
+                 |   *    |    *   |
+                 | Q2  *  | *   Q3 |
+                 +--------+--------+
 
-                ( Q1 | Q0)
-                 --------
-                ( Q2 | Q3)
+           NB may exploit image symmetry, all quadrants are equivalent, co-add
+           (1) quad.any() = False
+                (FALSE,FALSE,FALSE,FALSE) => inverse Abel transform for each quadrant
+
+               inverse image   AQ1 | AQ0
+                               ---------
+                               AQ2 | AQ3
+
+           (2) quad.any() = True   exploits image symmetry to improve signal
+                sum True quadrants Q = Q0 + Q1 + Q2 + Q3  (True,True,True,True)
+                             or    Q = Q0 + Q1 + Q2       (True,True,True,False)
+                                   etc
+
+                inverse image   AQ | AQ
+                                -------
+                                AQ | AQ
+
+          - calc_speeds: boolean, evaluate speed profile
+          - verbose: boolean, more output, timings etc.
+          - freecpus: integer, parallel processing, use all cpus - freecpus (default 1)
     """  
     pool = mp.Pool(processes=mp.cpu_count()-freecpus) 
 
@@ -86,7 +134,7 @@ def iabel_hansenlaw (data,quad=(True,True,True,True),calc_speeds=True,verbose=Tr
 
         Q = Q0*quad[0]+Q1*quad[1]+Q2*quad[2]+Q3*quad[3]
 
-        if verbose: print ("HL: Calculating inverse Abel transform ... ",end='')
+        if verbose: print ("HL: Calculating inverse Abel transform ... ")
         # inverse Abel transform of combined quadrant, applied to each row
 
         AQ0 = pool.map(iabel_hansenlaw_transform,[Q[row] for row in range(N2)])
@@ -97,7 +145,7 @@ def iabel_hansenlaw (data,quad=(True,True,True,True),calc_speeds=True,verbose=Tr
         if verbose: print ("HL: Individual quadrants")
 
         # inversion of each quandrant, one row at a time
-        if verbose: print ("HL: Calculating inverse Abel transform ... ",end='')
+        if verbose: print ("HL: Calculating inverse Abel transform ... ")
         AQ0 = pool.map(iabel_hansenlaw_transform,[Q0[row] for row in range(N2)])
         AQ1 = pool.map(iabel_hansenlaw_transform,[Q1[row] for row in range(N2)])
         AQ2 = pool.map(iabel_hansenlaw_transform,[Q2[row] for row in range(N2)])
@@ -112,7 +160,7 @@ def iabel_hansenlaw (data,quad=(True,True,True,True),calc_speeds=True,verbose=Tr
 
     if calc_speeds:
         if verbose:
-            print('Generating speed distribution ...',end='')
+            print('Generating speed distribution ...')
             t1 = time()
 
         speeds = calculate_speeds(recon, N)
--- examples/example_hansenlaw.py
+++ examples/example_hansenlaw.py
@@ -1,48 +1,96 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-######################################################################
-#
-# example alternative inverse Abel transformation using the method of
-#    Hansen and Law J. Opt. Soc. Am A2, 510-520 (1985) 
-#
-# Stephen Gibson - Australian National University, Australia
-# Jason Gascooke - Flinders University, Australia
-#
-#  O2-ANU1024.txt.bz2 is a bzipped 1024x1024 ascii velocity-map
-#  image of O2- photodetachment at 454 nm
-######################################################################
-
- 
 from abel.hansenlaw import *
-from matplotlib.pylab import plt
+from abel.io import load_raw
+import scipy.misc
 
-# ascii image data: ANU O- photodetachment velocity-map image 1024x1024 pixel
-im = np.loadtxt("./data/O2-ANU1024.txt.bz2")
+# This example demonstrates Hansen and Law inverse Abel transform
+# of an image obtained using a velocity map imaging (VMI) photoelecton 
+# spectrometer to record the photoelectron angular distribution resulting 
+# from photodetachement of O2- at 454 nm.
+# This spectrum was recorded in 2010  
+# ANU / The Australian National University
+# J. Chem. Phys. 133, 174311 (2010) DOI: 10.1063/1.3493349
+
+# Before you start, centre of the NxN numpy array should be the centre
+#  of image symmetry
+#   +----+----+
+#   |    |    |
+#   +----o----+
+#   |    |    |
+#   + ---+----+
+
+# Specify the path to the file
+filename = 'data/O2-ANU1024.txt.bz2'
+
+# Name the output files
+name = filename.split('.')[0]
+print ("----",name)
+output_image = name + '_inverse_Abel_transform_HansenLaw.png'
+output_text  = name + '_speeds_HansenLaw.dat'
+output_plot  = name + '_comparison_HansenLaw.pdf'
+
+# Step 1: Load an image file as a numpy array
+print('Loading ' + filename)
+im = np.loadtxt(filename)
 (n,m) = np.shape(im)
+n2 = n//2   # half-image
+print ('image size {:d}x{:d}'.format(n,m))
 
-# Hansen and Law inverse Abel transformation
-reconHL, speedHL = iabel_hansenlaw (im,quad=(False,False,False,False),verbose=True,freecpus=1)
+# Step 2: perform the Hansen & Law transform!
+print('Performing Hansen and Law inverse Abel transform:')
 
-np.savetxt("speedHL.dat",speedHL)
+recon, speeds = iabel_hansenlaw (im,quad=(True,True,True,True),verbose=True,freecpus=1)
 
-# pretty plot
-fig = plt.figure(figsize=(10,4))
-ax1 = plt.subplot(121)
-ax2 = plt.subplot(122)
-title = "Hansen & Law"
-ax1.set_title (title,position=(0.4,1))
-# vmax from image excluding centre line
-ax1.imshow(reconHL,aspect=None,vmin=0,vmax=reconHL[:n//2-50,:n//2-50].max())
-ax1.set_ylim(ax1.get_ylim()[::-1])
-ax2.plot (speedHL/max(speedHL[250:]),'b-',label='Hansen&Law')
-ax2.axis(ymin=-0.5,ymax=1.5)
-ax2.set_title ("ANU O2- photodetachment at 454 nm - speed")
-plt.legend()
-plt.savefig("example_hansenlaw_O2.png")
+# save the transform in 8-bit format:
+scipy.misc.imsave(output_image,recon)
+
+# save the speed distribution
+np.savetxt(output_text,speeds)
+
+## Finally, let's plot the data
+
+# Set up some axes
+fig = plt.figure(figsize=(15,4))
+ax1 = plt.subplot(131)
+ax2 = plt.subplot(132)
+ax3 = plt.subplot(133)
+
+# Plot the raw data
+im1 = ax1.imshow(im,origin='lower',aspect='auto')
+fig.colorbar(im1,ax=ax1,fraction=.1,shrink=0.9,pad=0.03)
+ax1.set_xlabel('x (pixels)')
+ax1.set_ylabel('y (pixels)')
+ax1.set_title('velocity map image')
+
+# Plot the 2D transform
+im2 = ax2.imshow(recon,origin='lower',aspect='auto',vmin=0,vmax=recon[:n2-50,:n2-50].max())
+fig.colorbar(im2,ax=ax2,fraction=.1,shrink=0.9,pad=0.03)
+ax2.set_xlabel('x (pixels)')
+ax2.set_ylabel('y (pixels)')
+ax2.set_title('Hansen Law inverse Abel')
+
+# Plot the 1D speed distribution
+ax3.plot(speeds)
+ax3.axis(ymin=-50)
+ax3.set_xlabel('Speed (pixel)')
+ax3.set_ylabel('Intensity')
+ax3.set_title('Speed distribution')
+
+# Prettify the plot a little bit:
+plt.subplots_adjust(left=0.06,bottom=0.17,right=0.95,top=0.89,wspace=0.35,hspace=0.37)
+
+# Save a image of the plot
+plt.savefig(output_plot,dpi=150)
+
+# Show the plots
 plt.show()
