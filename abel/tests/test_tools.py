d218fc0502c0966fc39e0605cf89be7dc810bdf7
Roman Yurchak <rth@crans.org>
2015-11-30 17:05:25 +0100

    Add tests for the centering function

--- abel/tests/test_tools.py
+++ abel/tests/test_tools.py
@@ -8,6 +8,7 @@ import numpy as np
 from numpy.testing import assert_allclose, assert_equal
 
 from abel.tools import calculate_speeds, center_image
+from abel.benchmark import is_symmetric
 
 
 
@@ -39,3 +40,30 @@ def test_centering_function_shape():
         res = center_image(data, (ni//2, ni//2), n)
         yield assert_equal, res.shape, (n, n),\
                     'Centering preserves shapes for ni={}, n={}'.format(ni, n)
+
+
+def test_centering_function():
+    # ni -> original shape of the data is (ni, ni)
+    # n_c  -> the image center is (n_c, n_c)
+    # n  -> after the centering function, the array has a shape (n, n)
+
+    for (ni, n_c, n) in [(9, 5, 3),
+                         (9, 5, 4),
+                         ]:
+        arr = np.zeros((ni, ni))
+
+        if n % 2 == 1:
+            arr[n_c-1:n_c+2,n_c-1:n_c+2] = 1
+        else:
+            arr[n_c-1:n_c+1,n_c-1:n_c+1] = 1
+
+        res = center_image(arr, (n_c, n_c), n)
+        # The print statements  below can be commented after we fix the centering issue
+        print('Original array')
+        print(arr)
+        print('Centered array')
+        print(res)
+
+        yield assert_equal, is_symmetric(res), True,\
+            'Validating the centering function for ni={}, n_c={}, n={}'.format(ni, n_c, n)
+
