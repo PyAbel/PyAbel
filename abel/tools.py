aaa940f0bb24b22359892e0eb774b7844f9c9f0b
stggh <Stephen.Gibson@anu.edu.au>
2016-01-11 22:46:12 +1100

    reproject_image_into_polar() removed rmax code, not worth the confusion for a time saving of 20%. The function is quite fast in any case. STILL issue with AQ0 reprojection

--- abel/tools.py
+++ abel/tools.py
@@ -33,8 +33,7 @@ def calculate_speeds(IM, origin=None, Jacobian=False, dr=1, dt=None):
      """
     
     polarIM, r_grid, theta_grid = reproject_image_into_polar(IM, origin, 
-                                       Jacobian=Jacobian, rmax='circle', 
-                                                          dr=dr, dt=dt)
+                                       Jacobian=Jacobian, dr=dr, dt=dt)
     theta = theta_grid[0,:]  # theta coordinates
     r = r_grid[:,0]          # radial coordinates
 
@@ -43,7 +42,7 @@ def calculate_speeds(IM, origin=None, Jacobian=False, dr=1, dt=None):
         polarIM = polarIM*sintheta[np.newaxis, :]
         polarIM = polarIM*r[:,np.newaxis]
     
-    speeds = np.sum(polarIM, axis=1)
+    speeds = np.sum(polarIM, axis=1)[:polarIM.shape[1]]  # trim to image width
     
     return speeds, r
 
@@ -379,7 +378,7 @@ def center_image_by_slice(IM, slice_width=10, radial_range=(0,-1),
 # It is possible that there is a faster way to convert to polar coordinates.
 
 def reproject_image_into_polar(data, origin=None, Jacobian=False,
-                               rmax='corner', dr=1, dt=None):
+                               dr=1, dt=None):
     """Reprojects a 2D numpy array ("data") into a polar coordinate system.
     "origin" is a tuple of (x0, y0) relative to the bottom-left image corner,
     and defaults to the center of the image.
@@ -389,13 +388,6 @@ def reproject_image_into_polar(data, origin=None, Jacobian=False,
      - data:   rowsxcolums numpy array
      - origin: tuple, the coordinate of the image center, relative to bottom-left
      - Jacobian: boolean, include r intensity scaling in the coordinate transform
-     - rmax:   radius maximum, beyond which no projection is done
-               'corner' => sqrt((rows//2)**2+(cols//2)**2)
-                  - used to convert the whole cartesian image to polar
-               otherwise the minimum of the 1/2 width or 1/2 height of the image
-               = min((rows//2, cols//2)
-                  - used to convert only the circular region that fits within
-                    the image
      - dr: radial coordinate spacing for the grid interpolation
              tests show that there is not much point in going below 0.5
      - dt: angular coordinate spacing (in degrees)
@@ -415,30 +407,16 @@ def reproject_image_into_polar(data, origin=None, Jacobian=False,
     r, theta = cart2polar(x, y)               # convert (x,y) -> (r,θ)
                                               # note θ=0 is vertical
 
-    if rmax == 'corner':
-       rmax = r.max()
-    else:
-       # find largest circle that will fit into image
-       rmax = np.min(np.abs((origin[0], nx-origin[0], origin[1], ny-origin[1])))
-       if rmax < 1:
-           # origin is (0,0)
-           rmax = np.min((nx, ny))
-
-    nr = np.round(rmax/dr)
+    nr = np.round((r.max()-r.min())/dr)
          
-    if dt is not None:
-       nt = np.round((theta.max()-theta.min())/(np.pi*dt/180))  # dt in degrees
+    if dt is None:
+       nt = ny
     else:
-       if rmax == 'corner': 
-       # remap image to the same shape
-           nt = ny
-       else:
-           # save computation time, usually I(θ) varies slowly
-           nt = ny//2
+       nt = np.round((theta.max()-theta.min())/(np.pi*dt/180))  # dt in degrees
 
     # Make a regular (in polar space) grid based on the min and max r & theta
-    r_i     = np.linspace(    r.min(), rmax,        nr)
-    theta_i = np.linspace(theta.min(), theta.max(), nt)
+    r_i     = np.linspace(r.min(), r.max(), nr, endpoint=False)
+    theta_i = np.linspace(theta.min(), theta.max(), nt, endpoint=False)
     theta_grid, r_grid = np.meshgrid(theta_i, r_i)
 
     # Project the r and theta grid back into pixel coordinates
@@ -454,7 +432,6 @@ def reproject_image_into_polar(data, origin=None, Jacobian=False,
 
     if Jacobian:
         output = output*r_i[:,np.newaxis]
-
     return output, r_grid, theta_grid
 
 
