aaff74fa81af8cf1b1ba65279e5bd668de85e8b0
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2016-01-14 09:11:37 +1100

    example_all_O2.py corrected 1d->2d array flip for the onion wrapper function. Now inverts as in the past, with low center noise

--- examples/example_all_O2.py
+++ examples/example_all_O2.py
@@ -28,11 +28,12 @@ from time import time
 
 def iabel_onion_transform(Q0):
     # onion peel requires Q1 oriented image
-    return iabel_onion_peeling(Q0[::-1])[::-1]
+    return iabel_onion_peeling(Q0[:,::-1])[:,::-1]
 
 def iabel_basex_transform(Q0):
     # basex requires a whole image
     IM = put_image_quadrants((Q0, Q0, Q0, Q0), odd_size=True)
+    print ("basex uses whole image reconstructed from Q0 shape ",IM.shape)
     rows, cols = IM.shape
     center = (rows//2+rows%2, cols//2+cols%2)
     AIM = BASEX (IM, center, n=rows, verbose=True)
