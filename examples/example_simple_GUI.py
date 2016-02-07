a8b665ae20ffc3b6c53609f2b8ba383f56861f6a
stggh <Stephen.Gibson@anu.edu.au>
2016-02-07 11:05:48 +1100

    pep8 fixes for example_simple_GUI.py

--- examples/example_simple_GUI.py
+++ examples/example_simple_GUI.py
@@ -2,18 +2,10 @@
 
 # Illustrative GUI driving a small subset of PyAbel methods
 
-# code adapted from http://matplotlib.org/examples/user_interfaces/embedding_in_tk.html
+# code adapted from
+# http://matplotlib.org/examples/user_interfaces/embedding_in_tk.html
 
 import numpy as np
-
-import matplotlib
-matplotlib.use('TkAgg')
-
-from numpy import arange, sin, pi
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,\
-                                              NavigationToolbar2TkAgg
-
-from matplotlib.figure import Figure
 
 from abel.hansenlaw import iabel_hansenlaw
 from abel.tools.vmi import calculate_speeds, find_image_center_by_slice
@@ -25,6 +17,12 @@ else:
     import tkinter as tk
 from tkFileDialog import askopenfilename
 
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,\
+                                              NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
 # ------------
 
 root = tk.Tk()
@@ -33,6 +31,7 @@ root.wm_title("Simple GUI PyAbel")
 f = Figure(figsize=(5, 4), dpi=100)
 a = f.add_subplot(111)
 
+
 def _display():
     global IM, canvas
     f.clf()
@@ -40,16 +39,19 @@ def _display():
     a.imshow(IM, vmin=0)
     canvas.show()
 
+
 def _getfilename():
     global IM
     fn = askopenfilename()
     IM = np.loadtxt(fn)
     _display()
 
+
 def _center():
     global IM
     IM, offset = find_image_center_by_slice(IM)
     _display()
+
 
 def _transform():
     global IM, canvas
@@ -59,14 +61,15 @@ def _transform():
     a.imshow(AIM, vmin=0, vmax=AIM.max()/2)
     canvas.show()
 
+
 def _speed():
     global IM, canvas
     AIM = iabel_hansenlaw(IM)
     speed, radial = calculate_speeds(AIM)
     f.clf()
-    a = f.add_subplot(111) 
+    a = f.add_subplot(111)
     a.plot(radial, speed/speed[50:].max())
-    a.axis(xmax=500,ymin=-0.05)
+    a.axis(xmax=500, ymin=-0.05)
     canvas.show()
 
 
@@ -76,12 +79,15 @@ def _quit():
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
 
-# buttons 
-tk.Button(master=root, text='Load image file', command=_getfilename).pack(anchor=tk.W)
+# buttons
+tk.Button(master=root, text='Load image file', command=_getfilename)\
+   .pack(anchor=tk.W)
 tk.Button(master=root, text='center image', command=_center).pack(anchor=tk.N)
 tk.Button(master=root, text='raw image', command=_display).pack(anchor=tk.N)
-tk.Button(master=root, text='inverse Abel tansform', command=_transform).pack(anchor=tk.N)
-tk.Button(master=root, text='speed distribution', command=_speed).pack(anchor=tk.N)
+tk.Button(master=root, text='inverse Abel tansform', command=_transform)\
+   .pack(anchor=tk.N)
+tk.Button(master=root, text='speed distribution', command=_speed)\
+   .pack(anchor=tk.N)
 tk.Button(master=root, text='Quit', command=_quit).pack(anchor=tk.SW)
 
 # a tk.DrawingArea
