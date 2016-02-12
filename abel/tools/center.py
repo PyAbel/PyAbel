39383281db5b2653cbf0fbca1470861bb47c19ad
gilbert <ldes89150@gmail.com>
2016-02-12 13:19:19 -0600

    omit the leading underscore for the find_center_by_center_of_mass

--- abel/tools/center.py
+++ abel/tools/center.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.ndimage import center_of_mass
 
 
-def _find_center_by_center_of_mass(data, verbose=True, **kwargs):
+def find_center_by_center_of_mass(data, verbose=True, **kwargs):
     com = center_of_mass(data)
     com_round = np.around(com)
     if verbose:
@@ -11,13 +11,13 @@ def _find_center_by_center_of_mass(data, verbose=True, **kwargs):
     return (int(com_round[1]), int(com_round[0]))
 
 
-def _find_center_by_center_of_image(data, verbose=True, **kwargs):
+def find_center_by_center_of_image(data, verbose=True, **kwargs):
     return (data.shape[1]//2, data.shape[0]//2)
 
 
 func_method = {
-    "auto": _find_center_by_center_of_image,
-    "com": _find_center_by_center_of_mass,
+    "auto": find_center_by_center_of_image,
+    "com": find_center_by_center_of_mass,
 }
 
 
