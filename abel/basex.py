0d06e616e3bc911686850b6f84598698cf275206
DhrubajyotiDas <ddas328@gmail.com>
2016-02-16 17:25:20 -0500

    Removing old vertical-symmetry-only code

--- abel/basex.py
+++ abel/basex.py
@@ -127,54 +127,12 @@ def _abel_basex_core(data, center='image_center', n='auto',
     # make sure n_horz is odd
     n[1] = 2 * (n[1] // 2) + 1 
 
-    if vertical_symmetry:
-        image = center_image(data, center=center, n=n[1], ndim=data_ndim)
-
-        if verbose:
-            t1 = time()
-
-        M, Mc, M_left, M_right = get_bs_basex_cached(n[1], nbf, basis_dir, verbose)
-
-        if verbose:
-            print('{:.2f} seconds'.format((time() - t1)))
-
-        # Do the actual transform
-        if verbose:
-            print('Reconstructing image...         ')
-            t1 = time()
-
-        recon = basex_transform(image, M, Mc, M_left, M_right, dr)
-
-        if verbose:
-            print('%.2f seconds' % (time() - t1))
-
-        if data_ndim == 1:
-            recon = recon[0, :] # taking one row, since they are all the same anyway
-
-        if calc_speeds:
-            if verbose:
-                print('Generating speed distribution...')
-                t1 = time()
-
-            speeds, r = calculate_speeds(recon)
-
-            if verbose:
-                print('%.2f seconds' % (time() - t1))
-            return recon, speeds
-        else:
-            return recon
-
-    else: # No vertical symmetry
-        if type(center) == tuple: cx, cy = center
-        elif type(center) == int: cx = center
-        else: raise ValueError("Center specified incorrectly. Must be tuple (x,y) or integer (column)")
-
         image = center_image_asym(data, center_column = cx, n_vert = n[0], n_horz = n[1], verbose = verbose)
 
         if verbose:
             t1 = time()
 
-        M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right = get_bs_basex_cached_asym(n_vert = n[0], n_horz = n[1], nbf = nbf, basis_dir = basis_dir, verbose = verbose)
+    M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right = get_bs_basex_cached(n_vert = n[0], n_horz = n[1], nbf = nbf, basis_dir = basis_dir, verbose = verbose)
 
         if verbose:
             print('{:.2f} seconds'.format((time() - t1)))
@@ -210,38 +168,7 @@ def _abel_basex_core(data, center='image_center', n='auto',
 
         return recon
 
-
-
-
-def basex_transform(rawdata, M, Mc, M_left, M_right, dr=1.0):
-    """ This is the internal function that does the actual BASEX transform
-
-     Parameters
-     ----------
-      - rawdata: a NxN ndarray of the raw image.
-      - M, Mc, M_left, M_right: 2D arrays given given by the basis set calculation function
-      - dr: float: pixel size
-
-     Returns
-     -------
-      IM: The abel-transformed image, a slice of the 3D distribution
-    """
-
-    Ci = M_left.dot(rawdata).dot(M_right)
-    # P = dot(dot(Mc,Ci),M.T) # This calculates the projection, which should recreate the original image
-    IM = Mc.dot(Ci).dot(Mc.T)
-
-    # use an heuristic scaling factor to match the analytical abel transform
-    # see https://github.com/PyAbel/PyAbel/issues/4
-    # When nbf = n//2, we need the MAGIC_NUMBER = 8.053
-    # When nbf = n//2 + 1, see below,
-    MAGIC_NUMBER = 1.1122244156826457 
-    # MAGIC_NUMBER = 8.053 
-    IM *= MAGIC_NUMBER/dr
-
-    return IM
-
-def basex_transform_asym(rawdata, M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right, dr=1.0):
+def basex_transform(rawdata, M_vert, M_horz, Mc_vert, Mc_horz, vert_left, horz_right, dr=1.0):
     """ This is the internal function that does the actual BASEX transform for the no-up/down-symmetry case.
      Parameters
      ----------
@@ -268,22 +195,7 @@ def basex_transform_asym(rawdata, M_vert, M_horz, Mc_vert, Mc_horz, vert_left, h
 
     return IM
 
-
-def _get_left_right_matrices(M, Mc):
-    """ An internal helper function for BASEX:
-        given basis sets  M, Mc return M_left and M_right matrices 
-        M_left and M_right are the A and B matrices (respectively) 
-        from Equation 13 of the Dribinski et al. paper."""
-
-    M_left = inv(Mc.T.dot(Mc)).dot(Mc.T) 
-    q = 1  # q is the regularization factor from Equation 8 of the Dribinski paper.
-           # Changing this from 1 may improve performance in some situations?
-    nbf = M.shape[1]        # number of basis functions
-    E = np.identity(nbf)*q  # Creating diagonal matrix for regularization. (?)
-    M_right = M.dot(inv((M.T.dot(M) + E)))
-    return M_left, M_right
-
-def _get_left_right_matrices_asym(M_vert, M_horz, Mc_vert, Mc_horz): 
+def _get_left_right_matrices(M_vert, M_horz, Mc_vert, Mc_horz): 
     """ An internal helper function for no-up/down-asymmetry BASEX:
         given basis sets  M_vert, M_horz, Mc_vert, Mc_horz,
         return M_left and M_right matrices 
@@ -299,20 +211,7 @@ def _get_left_right_matrices_asym(M_vert, M_horz, Mc_vert, Mc_horz):
 
     return vert_left, horz_right
 
-def _nbf_default(n, nbf):
-    """ An internal helper function to check that nbf = n//2 + 1 and print a warning
-    otherwise """
-    if nbf == 'auto':
-        nbf = n//2 + 1
-    else:
-        if nbf != n//2 +1:
-            print('Warning: the number of basis functions nbf = {} != (n//2 + 1) = {}\n'.format(n, nbf),
-                    '    This behaviour is currently not tested and should not be used\
-                    unless you know exactly what you are doing. Setting nbf="auto" is best for now.')
-
-    return nbf
-
-def _nbf_default_asym(n_vert, n_horz, nbf):
+def _nbf_default(n_vert, n_horz, nbf):
     """ An internal helper function for the asymmetric case to check that nbf = n//2 + 1 and print a warning otherwise """
     if nbf == 'auto':
         # nbf_vert = n_vert (if relevant)
@@ -334,67 +233,7 @@ def _nbf_default_asym(n_vert, n_horz, nbf):
         raise ValueError('nbf must be set to "auto" or an integer or a list')
     return nbf
 
-
-def get_bs_basex_cached(n, nbf='auto', basis_dir='.', verbose=False):
-    """
-    Internal function.
-
-    Get basis sets, using the disk as a cache 
-    (i.e. load from disk if they exist, if not calculate them and save a copy on disk).
-
-    Parameters:
-    -----------
-      - n : odd integer: Abel inverse transform will be performed on a `n x n`
-        area of the image
-      - nbf: integer: number of basis functions. If nbf='auto', it is set to (n//2 + 1).
-      - basis_dir : path to the directory for saving / loading the basis set coefficients.
-                    If None, the basis sets will not be saved to disk. 
-    """
-    # Sanitizing nbf
-    nbf = _nbf_default(n, nbf)
-
-    basis_name = "basex_basis_{}_{}.npy".format(n, nbf)
-
-    M = None
-    if basis_dir is not None:
-        path_to_basis_file = os.path.join(basis_dir, basis_name)
-        if os.path.exists(path_to_basis_file):
-            if verbose:
-                print('Loading basis sets...           ')
-                # saved as a .npy file
-            try:
-                M, Mc, M_left, M_right, M_version  = np.load(path_to_basis_file)
-            except ValueError:
-                try: # falling back to the legacy basis set file format
-                    M_left, M_right, M, Mc = np.load(path_to_basis_file)
-                except:
-                    raise
-            except:
-                raise
-
-    if M is None:
-        if verbose:
-            print('A suitable basis set was not found.',
-                  'A new basis set will be generated.',
-                  'This may take a few minutes. ', end='')
-            if basis_dir is not None:
-                print('But don\'t worry, it will be saved to disk for future use.\n')
-            else:
-                print(' ')
-
-        M, Mc = generate_basis_sets(n, nbf, verbose=verbose)
-        M_left, M_right = _get_left_right_matrices(M, Mc)
-
-        if basis_dir is not None:
-            np.save(path_to_basis_file,
-                    (M, Mc, M_left, M_right,  np.array(__version__)))
-            if verbose:
-                print('Basis set saved for later use to,')
-                print(' '*10 + '{}'.format(path_to_basis_file))
-    return M, Mc, M_left, M_right
-
-
-def get_bs_basex_cached_asym(n_vert, n_horz, nbf='auto', basis_dir='.', verbose=False):
+def get_bs_basex_cached(n_vert, n_horz, nbf='auto', basis_dir='.', verbose=False):
     """
     Internal function.
 
@@ -457,113 +296,7 @@ def get_bs_basex_cached_asym(n_vert, n_horz, nbf='auto', basis_dir='.', verbose=
 
 MAX_BASIS_SET_OFFSET = 4000
 
-
-def generate_basis_sets(n, nbf='auto', verbose=False):
-    """ 
-    Generate the basis set for the BASEX method. 
-
-    This function was adapted from the matlab script provided by
-    the Reisler group: BASIS2.m, with some optimizations.
-    
-    See original Matlab program here:
-    https://github.com/PyAbel/PyAbelLegacy/tree/master/Matlab
-    
-    Typically, the number of basis functions will be (n//2 + 1)
-    so that each pixel in the image is represented by its own basis function.
-
-    Parameters:
-    -----------
-      n : integer : size of the basis set (pixels)
-      nbf: integer: number of basis functions. If nbf='auto', it is set to (n//2 + 1).
-      verbose: bool: print progress 
-
-    Returns:
-    --------
-      M, Mc : np.array 2D:  basis sets
-    """
-    if n % 2 == 0:
-        raise ValueError('The n parameter must be odd (more or less sure about it).')
-
-    nbf = _nbf_default(n, nbf)
-
-    if nbf > n//2 + 1:
-        raise ValueError('The number of basis functions nbf cannot be larger then the number of points n!')
-
-    Rm = n//2 + 1
-
-    I = np.arange(1, n+1)
-
-    R2 = (I - Rm)**2
-    # R = I - Rm
-    M = np.zeros((n, nbf))
-    Mc = np.zeros((n, nbf))
-
-    M[:,0] = 2*np.exp(-R2)
-    Mc[:,0] = np.exp(-R2)
-
-    gammaln_0o5 = gammaln(0.5) 
-
-    if verbose:
-        print('Generating BASEX basis sets for n = {}, nbf = {}:\n'.format(n, nbf))
-        sys.stdout.write('0')
-        sys.stdout.flush()
-
-    # the number of elements used to calculate the projected coefficients
-    delta = np.fmax(np.arange(nbf)*32 - MAX_BASIS_SET_OFFSET, MAX_BASIS_SET_OFFSET)
-    for k in range(1, nbf):
-        k2 = k*k # so we don't recalculate it all the time
-        log_k2 = log(k2) 
-        angn = exp(
-                    k2 - 2*k2*log(k) +
-                    #np.log(np.arange(0.5, k2 + 0.5)).sum() # original version
-                    gammaln(k2 + 0.5) - gammaln_0o5  # optimized version
-                    )
-        M[Rm-1, k] =  2*angn
-
-        for l in range(1, n-Rm+1):
-            l2 = l*l
-            log_l2 = log(l2)
-
-            val = exp(k2 - l2 + 2*k2*log((1.0*l)/k))
-            Mc[l-1+Rm, k] = val
-            Mc[Rm-l-1, k] = val
-
-            aux = val + angn*Mc[l+Rm-1, 0]
-
-            p = np.arange(max(1, l2 - delta[k]), min(k2 - 1,  l2 + delta[k]) + 1)
-
-            # We use here the fact that for p, k real and positive
-            #
-            #  np.log(np.arange(p, k)).sum() == gammaln(k) - gammaln(p) 
-            #
-            # where gammaln is scipy.misc.gammaln (i.e. the log of the Gamma function)
-            #
-            # The following line corresponds to the vectorized third
-            # loop of the original BASIS2.m matlab file.
-
-            aux += np.exp(k2 - l2 - k2*log_k2 + p*log_l2
-                      + gammaln(k2+1) - gammaln(p+1) 
-                      + gammaln(k2 - p + 0.5) - gammaln_0o5
-                      - gammaln(k2 - p + 1)
-                      ).sum()
-
-            # End of vectorized third loop
-
-            aux *= 2
-
-            M[l+Rm-1, k] = aux
-            M[Rm-l-1, k] = aux
-
-        if verbose and k % 50 == 0:
-            sys.stdout.write('...{}'.format(k))
-            sys.stdout.flush()
-
-    if verbose:
-        print("...{}".format(k+1))
-
-    return M, Mc
-
-def _bs_basex_asym(n_vert=1001, n_horz = 501, 
+def _bs_basex(n_vert=1001, n_horz = 501, 
                                     nbf_vert = 1001, nbf_horz = 251, 
                                     verbose=True):    
     """ 
