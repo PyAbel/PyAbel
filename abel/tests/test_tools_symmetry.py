c39520a7c4bd661954dc063d168fa38ae39719f6
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2016-02-18 10:52:57 +1100

    abel/tests/test_tools_symmetry.py added verbose print

--- abel/tests/test_tools_symmetry.py
+++ abel/tests/test_tools_symmetry.py
@@ -10,21 +10,37 @@ from numpy.testing import assert_allclose
 
 import abel
 
-def test_symmetry_get_put_quadrants():
+def test_symmetry_get_put_quadrants(verbose=False):
     
     z = np.zeros((5, 5))
+    z[0,0] = 1
+    z[-1,0] = 2
+    z[-1,-1] = 3
     z[1, 1] = 1
     z[1, -2] = 1
     z[-2, 1] = 1
     z[-2, -2]= 1
 
+    if verbose:
+        print("test image")
+        print(z)
+
     q = abel.tools.symmetry.get_image_quadrants(z, reorient=True)
 
+    if verbose:
+        for i, qi in enumerate(q):
+            print("\nreoriented quadrant Q{:d}".format(i))
+            print(qi)
+
     r = abel.tools.symmetry.put_image_quadrants(q)
+
+    if verbose:
+        print("\nreassembled image")
+        print(r)
 
 
     assert not (z-r).any()
 
 
 if __name__ == "__main__":
-  test_symmetry_get_put_quadrants()
+  test_symmetry_get_put_quadrants(verbose=True)
