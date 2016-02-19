e3ef67a831d7e8120269ffa96d228a6c1c8596b0
DhrubajyotiDas <ddas328@gmail.com>
2016-02-19 00:37:21 -0500

    io.py conforms to PEP8

--- abel/tools/io.py
+++ abel/tools/io.py
@@ -15,29 +15,34 @@ import numpy as np
 
 def load_raw(filename, start=2, end=1440746, height=1038, width=1388):
     """
-     This loads raw VMI images from Vrakking's "VMI_Acquire" software
-     It ignores the first two values (which are just the dimensions of the image,
-     and not actual data) and cuts off about 10 values at the end.
-     It's unclear why the files are not quite the right size, but this seems to work.
+    This loads raw VMI images from Vrakking's "VMI_Acquire" software
+    It ignores the first two values
+    (which are just the dimensions of the image, and not actual data)
+    and cuts off about 10 values at the end.
+    It's unclear why the files are not quite the right size,
+    but this seems to work.
     """
-    
+
     # Load raw data
     A = np.fromfile(filename, dtype='int32', sep="")
     # Reshape into a numpy array
     return A[start:end].reshape([height, width])
-    
+
 
 def save16bitPNG(filename, data):
-    """ It's not easy to save 16-bit images in Python. Here is a way to save a 16-bit PNG
-     Again, this is thanks to stackoverflow: 
-     http://stackoverflow.com/questions/25696615/can-i-save-a-numpy-array-as-a-16-bit-image-using-normal-enthought-python
-     This requires pyPNG
     """
-    
+    It's not easy to save 16-bit images in Python.
+    Here is a way to save a 16-bit PNG
+    Again, this is thanks to stackoverflow:
+    http://stackoverflow.com/questions/25696615/\
+    can-i-save-a-numpy-array-as-a-16-bit-image-using-normal-enthought-python
+    This requires pyPNG
+    """
+
     import png
     with open(filename, 'wb') as f:
         writer = png.Writer(width=data.shape[1], height=data.shape[0],
-                                bitdepth=16, greyscale=True)
+                            bitdepth=16, greyscale=True)
         data_list = data.tolist()
         writer.write(f, data_list)
 
