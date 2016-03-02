043bcf98433f1e197c5002d8549169f6466b8536
DhrubajyotiDas <ddas328@gmail.com>
2016-03-02 16:26:12 -0500

    Chopped test image in half before passing to three_point_transform()

--- examples/example_three_point.py
+++ examples/example_three_point.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import abel 
 
 n = 101
+center = n//2
 r_max = 4
 r = np.linspace(-1*r_max, r_max, n)
 dr = r[1]-r[0]
@@ -18,14 +19,18 @@ axes[1].set_ylabel("f(r)")
 axes[1].set_title("Inverted radial signal")
 
 Mat = np.sqrt(np.pi)*np.exp(-1*r*r)
+Left_Mat = Mat[:center+1][::-1]
+Right_Mat = Mat[center:]
 AnalyticAbelMat = np.exp(-1*r*r)
-DaschAbelMat = abel.three_point.three_point_transform(Mat)[0]
+DaschAbelMat_Left = abel.three_point.three_point_transform(Left_Mat)[0]/dr
+DaschAbelMat_Right = abel.three_point.three_point_transform(Right_Mat)[0]/dr
 
 axes[0].plot(r, Mat, 'r', label = r'$\sqrt{\pi}e^{-r^2}$')
 axes[0].legend()
 
 axes[1].plot(r, AnalyticAbelMat, 'k', lw = 1.5, alpha = 0.5, label = r'$e^{-r^2}$')
-axes[1].plot(r, DaschAbelMat, 'r--', lw = 1.5, label = '3-pt Abel')
+axes[1].plot(r[:center+1], DaschAbelMat_Left[::-1], 'r--.', label = '3-pt Abel')
+axes[1].plot(r[center:], DaschAbelMat_Right, 'r--.')
 
 box = axes[1].get_position()
 axes[1].set_position([box.x0, box.y0, box.width * 0.8, box.height])
