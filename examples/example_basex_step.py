ecc275eaac02df8f6a99dee65f697ca768bf3386
DhrubajyotiDas <ddas328@gmail.com>
2016-03-01 19:46:23 -0500

    Fixed BASEX example by splitting analytical Abel into two halves

--- examples/example_basex_step.py
+++ examples/example_basex_step.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from abel.basex import basex_transform
 from abel.tools.analytical import StepAnalytical
 
-# This example calculates the BASEX transform of a step function and 
+# This example calculates the BASEX transform of a step function and
 # compares with the analtical result.
-fig, ax= plt.subplots(1,1)
+fig, ax = plt.subplots(1, 1)
 plt.title('Abel tranforms of a step function')
 
 n = 501
@@ -17,22 +17,30 @@ r2 = 14.0
 # define a symmetric step function and calculate its analytical Abel transform
 st = StepAnalytical(n, r_max, r1, r2, A0)
 
-ax.plot(st.r, st.func,'b', label='Original signal')
+ax.plot(st.r, st.func, 'b', label='Original signal')
 
-ax.plot(st.r, st.abel*0.05, 'r', label='Direct Abel transform x0.05 [analytical]')
+ax.plot(
+    st.r, st.abel*0.05, 'r',
+    label='Direct Abel transform x0.05 [analytical]')
 
 center = n//2
-# BASEX Transform: 
+right_half = st.abel[center:]
+left_half = st.abel[:center+1][::-1]
+# BASEX Transform:
 # Calculate the inverse abel transform for the centered data
-recon = basex_transform(st.abel, basis_dir='./', dr=st.dr,
-                                 verbose=True)
+recon_right = basex_transform(
+    right_half, basis_dir='./', dr=st.dr, verbose=True)
+recon_left = basex_transform(
+    left_half, basis_dir='./', dr=st.dr, verbose=False)
+plt.plot(
+    st.r[center:], recon_right, '--.', c='red',
+    label='Inverse transform [BASEX]')
 
-
-plt.plot(st.r, recon , '--o',c='red', label='Inverse transform x10 [BASEX]')
+plt.plot(st.r[:center+1], recon_left[::-1], '--.', c='red')
 
 ax.legend()
 
-ax.set_xlim(-20,20)
+ax.set_xlim(-20, 20)
 ax.set_ylim(-5, 20)
 ax.set_xlabel('x')
 ax.set_ylabel("f(x)")
