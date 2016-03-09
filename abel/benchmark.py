53803bb1cc0fde2a600e4cb14bcde11d26b1cd63
DanHickstein <danhickstein@gmail.com>
2016-03-09 08:47:09 -0700

    Made abel.benchmark.AbelTiming print in a more "importable" format

--- abel/benchmark.py
+++ abel/benchmark.py
@@ -13,7 +13,7 @@ from . import three_point
 from . import tools
 
 class AbelTiming(object):
-    def __init__(self, n=[301, 501], n_max_bs=700, transform_repeat=1):
+    def __init__(self, n=[301, 501], n_max_bs=700, n_max_slow=700, transform_repeat=1):
         """
         Benchmark performance of different iAbel/fAbel implementations.
 
@@ -25,75 +25,80 @@ class AbelTiming(object):
             since the basis sets generation takes a long time,
             do not run this benchmark for implementations that use basis sets
             for n > n_max_bs
+        n_max_slow: integer
+            maximum n run for the "slow" transform methods, so far including only
+            the "direct_python" implementation.
         """
         from timeit import Timer
 
         self.n = n
 
 
-        res_fabel = {'HansenLaw':     {'tr': []},
-                     'direct_Python': {'tr': []}}
-        res_iabel = {'BASEX':         {'bs': [], 'tr': []},
-                     'Three_point':   {'bs': [], 'tr': []},
-                     'HansenLaw':     {'tr': []},
-                     'direct_Python': {'tr': []}}
-        if direct.cython_ext:
-            res_fabel['direct_C'] = {'tr': []}
-            res_iabel['direct_C'] = {'tr': []}
+        # res_fabel = {'HansenLaw':     {'tr': []},
+        #              'direct_Python': {'tr': []}}
+        # res_iabel = {'BASEX':         {'bs': [], 'tr': []},
+        #              'Three_point':   {'bs': [], 'tr': []},
+        #              'HansenLaw':     {'tr': []},
+        #              'direct_Python': {'tr': []}}
 
+        
+        res_fabel = {'hansenlaw':      [],
+                     'direct_python':  [] }
+        res_iabel = {'basex':          [],
+                     'basex_bs':       [],
+                     'three_point':    [],
+                     'three_point_bs': [],
+                     'hansenlaw':      [],
+                     'direct_python' : [] }
+                     
+        if direct.cython_ext:
+            res_fabel['direct_C'] = []
+            res_iabel['direct_C'] = []
+            
         for ni in n:
             x = np.random.randn(ni, ni)
-            # direct implementations
+            
             if ni <= n_max_bs:
                 # evaluate basis sets, saved for later re-use
                 bs = basex.get_bs_basex_cached(ni, ni)
                 tbs = three_point.get_bs_three_point_cached(ni)
-                res_iabel['BASEX']['bs'].append(
-                    Timer(lambda: basex.get_bs_basex_cached(ni, ni, basis_dir=None)).
-                    timeit(number=1)*1000)
-                res_iabel['BASEX']['tr'].append(
-                    Timer(lambda: basex.basex_core_transform(x, *bs)).
-                    timeit(number=transform_repeat)*1000/transform_repeat)
-                res_iabel['Three_point']['bs'].append(
-                    Timer(lambda: three_point.get_bs_three_point_cached(ni, basis_dir=None)).
-                    timeit(number=1)*1000)
-                res_iabel['Three_point']['tr'].append(
-                    Timer(lambda: three_point.three_point_core_transform(x, tbs)).
-                          timeit(number=transform_repeat)*1000/transform_repeat)
+                res_iabel['basex_bs'].append(Timer(
+                    lambda: basex.get_bs_basex_cached(ni, ni, basis_dir=None)).timeit(number=1)*1000)
+                res_iabel['basex'   ].append(Timer(
+                    lambda: basex.basex_core_transform(x, *bs)).timeit(number=transform_repeat)*1000/transform_repeat)
+                res_iabel['three_point_bs'].append(Timer(
+                    lambda: three_point.get_bs_three_point_cached(ni, basis_dir=None)).timeit(number=1)*1000)
+                res_iabel['three_point'].append(Timer(
+                    lambda: three_point.three_point_core_transform(x, tbs)).timeit(number=transform_repeat)*1000/transform_repeat)
             else:
-                res_iabel['BASEX']['bs'].append(np.nan)
-                res_iabel['BASEX']['tr'].append(np.nan)
-                res_iabel['Three_point']['bs'].append(np.nan)
-                res_iabel['Three_point']['tr'].append(np.nan)
-
-            res_fabel['HansenLaw']['tr'].append(
-                Timer(lambda: hansenlaw.hansenlaw_transform(
-                      x, direction='forward')).timeit(number=transform_repeat)*1000/transform_repeat)
+                res_iabel['basex_bs'].append(np.nan)
+                res_iabel['basex'   ].append(np.nan)
+                res_iabel['three_point_bs'].append(np.nan)
+                res_iabel['three_point'   ].append(np.nan)
+            
+            if ni <= n_max_slow:
+                res_iabel['direct_python'].append(Timer(
+                    lambda: direct.direct_transform(x, backend='Python', direction='inverse')).timeit(number=transform_repeat)*1000/transform_repeat)
                       
-            res_iabel['HansenLaw']['tr'].append(
-                Timer(lambda: hansenlaw.hansenlaw_transform(
-                      x, direction='inverse')).timeit(number=transform_repeat)*1000/transform_repeat)
+                res_fabel['direct_python'].append(Timer(
+                    lambda: direct.direct_transform(x, backend='Python', direction='forward')).timeit(number=transform_repeat)*1000/transform_repeat)
+            
+            else:
+                res_iabel['direct_python'].append(np.nan)
+                res_fabel['direct_python'].append(np.nan)
+                
+            res_fabel['hansenlaw'].append(Timer(
+            lambda: hansenlaw.hansenlaw_transform(x, direction='forward')).timeit(number=transform_repeat)*1000/transform_repeat)
                       
-            res_iabel['direct_Python']['tr'].append(
-                Timer(lambda: direct.direct_transform(
-                      x, backend='Python',
-                      direction='inverse')).timeit(number=transform_repeat)*1000/transform_repeat)
-                      
-            res_fabel['direct_Python']['tr'].append(
-                Timer(lambda: direct.direct_transform(
-                      x, backend='Python',
-                      direction='forward')).timeit(number=transform_repeat)*1000/transform_repeat)
+            res_iabel['hansenlaw'].append(Timer(
+                lambda: hansenlaw.hansenlaw_transform(x, direction='inverse')).timeit(number=transform_repeat)*1000/transform_repeat)
                       
             if direct.cython_ext:
-                res_iabel['direct_C']['tr'].append(
-                    Timer(lambda: direct.direct_transform(
-                        x, backend='C',
-                        direction='inverse')).timeit(number=transform_repeat)*1000/transform_repeat)
+                res_iabel['direct_C'].append(Timer(
+                    lambda: direct.direct_transform(x, backend='C', direction='inverse')).timeit(number=transform_repeat)*1000/transform_repeat)
                         
-                res_fabel['direct_C']['tr'].append(
-                    Timer(lambda: direct.direct_transform(
-                        x, backend='C',
-                        direction='forward')).timeit(number=transform_repeat)*1000/transform_repeat)
+                res_fabel['direct_C'].append(Timer(
+                    lambda: direct.direct_transform(x, backend='C', direction='forward')).timeit(number=transform_repeat)*1000/transform_repeat)
 
         self.fabel = res_fabel
         self.iabel = res_iabel
@@ -105,36 +110,25 @@ class AbelTiming(object):
         out = []
         out += ['PyAbel benchmark run on {}\n'.format(platform.processor())]
 
-        LABEL_FORMAT = '|'.join([' Implementation '] +
-                                ['    n = {:<12} '.
-                                format(ni) for ni in self.n])
-        TR_ROW_FORMAT = '|'.join(['{:>15} '] + [' {:8.1f}            ']\
-                                 * len(self.n))
-        BS_ROW_FORMAT = '|'.join(['{:>15} '] + [' {:8.1f} ({:8.1f}) ']\
-                                 * len(self.n))
-        SEP_ROW = ' ' + '-'*(22 + (17+1)*len(self.n))
+        LABEL_FORMAT = 'Implementation ' + ''.join( ['    n = {:<12} '.format(ni) for ni in self.n])
+        ROW_FORMAT = '{:>15} ' + ' {:8.1f}            '*len(self.n)
+        SEP_ROW = '' + '-'*(22 + (17+1)*len(self.n))
 
-        HEADER_ROW = ' ========= {:>10} Abel implementations ==========\n' \
-                     'time to solution [milli-sec (=sec/1000)] -> transform'\
-                     ' (basis sets generation)\n'
+        HEADER_ROW = '\n========= {:>10} Abel implementations ==========\n' \
+                     'time to solution (sec)\n'
 
         def print_benchmark(name, res):
             out = [HEADER_ROW.format(name)]
             if res:
                 out += [LABEL_FORMAT]
                 out += [SEP_ROW]
-                for name, row in res.items():
-                    if 'bs' in row:
-                        pars = list(chain(*zip(row['tr'], row['bs'])))
-                        out += [BS_ROW_FORMAT.format(name, *pars)]
-                    else:
-                        out += [TR_ROW_FORMAT.format(name, *row['tr'])]
+                for name, row in sorted(res.items()):
+                    out += [ROW_FORMAT.format(name, *row)]
             return out
 
         out += print_benchmark('Direct', self.fabel)
         out += ['']
         out += print_benchmark('Inverse', self.iabel)
-
         return '\n'.join(out)
 
 
