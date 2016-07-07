21d3982181269a7a4d74146e51043efd78f14034
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2016-07-07 16:46:12 +1000

    updated example_PAD.py

--- examples/example_PAD.py
+++ examples/example_PAD.py
@@ -12,14 +12,28 @@ import matplotlib.pylab as plt
 IM = np.loadtxt("data/O2-ANU1024.txt.bz2")
 # use scipy.misc.imread(filename) to load image formats (.png, .jpg, etc)
 
-# Hansen & Law inverse Abel transform
-AIM = abel.Transform(IM, center="slice", method="hansenlaw",
-                     symmetry_axis=None).transform
+#=== linbasex transform ===================================
+legendre_orders = [0, 2, 4]  # Legendre polynomial orders
+proj_angles = range(0, 180, 10)  # projection angles in 10 degree steps
+radial_step = 1  # pixel grid
+smoothing = 1  # smoothing 1/e-width for Gaussian convolution smoothing
+threshold = 0.2  # threshold for normalization of higher order Newton spheres
+clip=0  # clip first vectors (smallest Newton spheres) to avoid singularities
 
-# PES - photoelectron speed distribution  -------------
-print('Calculating speed distribution:')
+# linbasex method - center and center_options ensure image has odd square shape
+LIM = abel.Transform(IM, method='linbasex', center='slice',
+                     center_options=dict(square=True),
+                     transform_options=dict(basis_dir=None,
+                     proj_angles=proj_angles, radial_step=radial_step,
+                     smoothing=smoothing, threshold=threshold, clip=clip,
+                     return_Beta=True, verbose=True))
 
-r, speed  = abel.tools.vmi.angular_integration(AIM)
+
+# === Hansen & Law inverse Abel transform ==================
+HIM = abel.Transform(IM, center="slice", method="hansenlaw",
+                     symmetry_axis=None, angular_integration=True)
+
+radial, speed = HIM.angular_integration
 
 # normalize to max intensity peak
 speed /= speed[200:].max()  # exclude transform noise near centerline of image
@@ -27,15 +41,16 @@ speed /= speed[200:].max()  # exclude transform noise near centerline of image
 # PAD - photoelectron angular distribution  ------------
 # radial ranges (of spectral features) to follow intensity vs angle
 # view the speed distribution to determine radial ranges
-r_range = [(93, 111), (145, 162), (255, 280), (330, 350), (350, 370), 
-           (370, 390), (390, 410), (410, 430)]
+r_range = [(145, 162), (200, 218), (230, 250), (255, 280), (280, 310), 
+           (310, 330), (330, 350), (350, 370), (370, 390), (390, 410),
+           (410, 430)]
 
 # anisotropy parameter for each tuple r_range
-Beta, Amp, R = abel.tools.vmi.anisotropy(AIM, r_range)
+Beta, Amp, R = abel.tools.vmi.anisotropy(HIM.transform, r_range)
 
 # OR  anisotropy parameter for ranges (0, 20), (20, 40) ...
-Beta_whole_grid, Amp_whole_grid, Radial_midpoints =\
-                         abel.tools.vmi.anisotropy(AIM, 20)
+#Beta_whole_grid, Amp_whole_grid, Radial_midpoints =\
+#                         abel.tools.vmi.anisotropy(AIM.transform, 20)
 
 # plots of the analysis
 fig = plt.figure(figsize=(8, 4))
@@ -46,11 +61,11 @@ ax2 = plt.subplot(122)
 rows, cols = IM.shape
 c2 = cols//2
 vmax = IM[:, :c2-100].max()
+AIM = HIM.transform
 AIM *= vmax/AIM[:, c2+100:].max()
 JIM = np.concatenate((IM[:, :c2], AIM[:, c2:]), axis=1)
 
-# Prettify the plot a little bit:
-# Plot the raw data
+# Plot the image data VMI | inverse Abel
 im1 = ax1.imshow(JIM, origin='lower', aspect='auto', vmin=0, vmax=vmax)
 fig.colorbar(im1, ax=ax1, fraction=.1, shrink=0.9, pad=0.03)
 ax1.set_xlabel('x (pixels)')
@@ -59,10 +74,12 @@ ax1.set_title('VMI, inverse Abel: {:d}x{:d}'\
               .format(rows, cols))
 
 # Plot the 1D speed distribution
-ax2.plot(speed, label='speed')
-ax2.errorbar(R, Beta[0], Beta[1], fmt='o', color='r', label='specific radii')
-ax2.plot(Radial_midpoints, Beta_whole_grid[0], '-g', label='stepped')
-ax2.axis(xmax=450, ymin=-1.2, ymax=1.2)
+ax2.plot(LIM.Beta[0], 'r-', label='linbasex-Beta[0]')
+ax2.plot(speed, 'b-', label='speed')
+ax2.plot(LIM.Beta[1], 'r-', label='linbasex-Beta[2]')
+ax2.errorbar(R, Beta[0], Beta[1], fmt='o', color='g', label='specific radii')
+#ax2.plot(Radial_midpoints, Beta_whole_grid[0], '-g', label='stepped')
+ax2.axis(xmin=100, xmax=450, ymin=-1.2, ymax=1.2)
 ax2.set_xlabel('radial pixel')
 ax2.set_ylabel('speed/anisotropy')
 ax2.set_title('speed/anisotropy distribution')
