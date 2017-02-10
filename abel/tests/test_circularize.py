682ae9b3b70000e64f49054d572de3999bca6599
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2017-02-10 14:47:37 +1100

    rebase

---
+++ abel/tests/test_circularize.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+from numpy.testing import assert_equal, assert_almost_equal
+
+import abel
+
+
+def test_circularize_image():
+
+    IM = abel.tools.analytical.sample_image(n=511, name='Ominus', sigma=2)
+
+    IMdist = abel.tools.analytical.flower_distort(IM)
+
+    nslices = 32
+
+    IMcirc, angle, scalefactor, spline =\
+        abel.tools.circularize.circularize_image(IMdist,
+                   method='lsq', nslices=nslices, zoom=1, smooth=0,
+                   ref_angle=0, return_correction=True)
+
+    r, c = IMcirc.shape
+
+    diff = (IMcirc - IM).sum(axis=1).sum(axis=0)
+
+    assert_almost_equal(diff, 425854.7, decimal=0)
+
+    assert_equal(len(angle), nslices)
+
+    assert_almost_equal(angle[-1], 3.04, decimal=2)
+
+    assert_almost_equal(scalefactor[4], 0.92, decimal=2)
+
+
+if __name__ == "__main__":
+    test_circularize_image()
