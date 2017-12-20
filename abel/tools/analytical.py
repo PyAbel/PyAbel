ec79fa619a5ea555a86f9c25edcaf4c7af235693
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2017-12-21 10:02:51 +1100

    fix test_benchmarks.py

--- abel/tools/analytical.py
+++ abel/tools/analytical.py
@@ -97,72 +97,72 @@ class StepAnalytical(BaseAnalytical):
 
         self.func = fr
 
-        self.abel = sym_abel_step_1d(self.r, self.A0, self.r1, self.r2)[0]
+        self.abel = self.sym_abel_step_1d(self.r, self.A0, self.r1, self.r2)[0]
 
         # exclude the region near the discontinuity
         self.mask_valid = np.abs(
             np.abs(self.r) - 0.5*(r1 + r2)) < ratio_valid_step*0.5*(r2 - r1)
 
 
-        def abel_step_analytical(r, A0, r0, r1):
-            """
-            Directed Abel transform of a step function located between r0 and r1,
-            with a height A0
+    def abel_step_analytical(self, r, A0, r0, r1):
+        """
+        Directed Abel transform of a step function located between r0 and r1,
+        with a height A0
 
-            ::
+        ::
 
-                A0 +                  +-------------+
-                   |                  |             |
-                   |                  |             |
-                 0 | -----------------+             +-------------
-                   +------------------+-------------+------------>
-                   0                  r0            r1           r axis
+            A0 +                  +-------------+
+               |                  |             |
+               |                  |             |
+             0 | -----------------+             +-------------
+               +------------------+-------------+------------>
+               0                  r0            r1           r axis
 
-            This function is mostly used for unit testing the inverse Abel transform
+        This function is mostly used for unit testing the inverse Abel transform
 
-            Parameters
-            ----------
-            r1 : 1D array,
-                vecor of positions along the r axis. Must start with 0.
-            r0, r1 : floats,
-                positions of the step along the r axis
-            A0 : float or 1D array:
-                height of the step. If 1D array the height can be variable
-                along the Z axis
+        Parameters
+        ----------
+        r1 : 1D array,
+            vecor of positions along the r axis. Must start with 0.
+        r0, r1 : floats,
+            positions of the step along the r axis
+        A0 : float or 1D array:
+            height of the step. If 1D array the height can be variable
+            along the Z axis
 
-            Returns
-            -------
-                1D array if A0 is a float, a 2D array otherwise
-            """
+        Returns
+        -------
+            1D array if A0 is a float, a 2D array otherwise
+        """
 
-            if np.all(r[[0, -1]]):
-                raise ValueError('The vector of r coordinates must start with 0.0')
+        if np.all(r[[0, -1]]):
+            raise ValueError('The vector of r coordinates must start with 0.0')
 
-            F_1d = np.zeros(r.shape)
-            mask = (r >= r0)*(r < r1)
-            F_1d[mask] = 2*np.sqrt(r1**2 - r[mask]**2)
-            mask = r < r0
-            F_1d[mask] = 2*np.sqrt(r1**2 - r[mask]**2) -\
-                         2*np.sqrt(r0**2 - r[mask]**2)
-            A0 = np.atleast_1d(A0)
-            if A0.ndim == 1:
-                A0 = A0[:, np.newaxis]
+        F_1d = np.zeros(r.shape)
+        mask = (r >= r0)*(r < r1)
+        F_1d[mask] = 2*np.sqrt(r1**2 - r[mask]**2)
+        mask = r < r0
+        F_1d[mask] = 2*np.sqrt(r1**2 - r[mask]**2) -\
+                     2*np.sqrt(r0**2 - r[mask]**2)
+        A0 = np.atleast_1d(A0)
+        if A0.ndim == 1:
+            A0 = A0[:, np.newaxis]
 
-            return F_1d[np.newaxis, :]*A0
+        return F_1d[np.newaxis, :]*A0
 
 
-        def sym_abel_step_1d(r, A0, r0, r1):
-            """
-            Produces a symmetrical analytical transform of a 1d step
-            """
-            A0 = np.atleast_1d(A0)
-            d = np.empty(A0.shape + r.shape)
-            A0 = A0[np.newaxis, :]
+    def sym_abel_step_1d(self, r, A0, r0, r1):
+        """
+        Produces a symmetrical analytical transform of a 1d step
+        """
+        A0 = np.atleast_1d(A0)
+        d = np.empty(A0.shape + r.shape)
+        A0 = A0[np.newaxis, :]
 
-            for sens, mask in enumerate([r >= 0, r <= 0]):
-                d[:, mask] = abel_step_analytical(np.abs(r[mask]), A0, r0, r1)
+        for sens, mask in enumerate([r >= 0, r <= 0]):
+            d[:, mask] = self.abel_step_analytical(np.abs(r[mask]), A0, r0, r1)
 
-            return d
+        return d
 
 
 class GaussianAnalytical(BaseAnalytical):
