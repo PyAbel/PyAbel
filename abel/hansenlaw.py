6687df9f8819cd91c506bb0c21fe4d1bffc2be82
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2018-02-02 12:00:32 +1100

    pep8 fixes

--- abel/hansenlaw.py
+++ abel/hansenlaw.py
@@ -8,22 +8,23 @@ import numpy as np
 
 #############################################################################
 # hansenlaw - a recursive method forward/inverse Abel transform algorithm
-# 
+#
 # Stephen Gibson - Australian National University, Australia
 # Jason Gascooke - Flinders University, Australia
-# 
+#
 # This algorithm is adapted by Jason Gascooke from the article
 #   E. W. Hansen and P-L. Law
 #  "Recursive methods for computing the Abel transform and its inverse"
 #   J. Opt. Soc. Am A2, 510-520 (1985) doi: 10.1364/JOSAA.2.000510
-# 
+#
 #  J. R. Gascooke PhD Thesis:
 #   "Energy Transfer in Polyatomic-Rare Gas Collisions and Van Der Waals
 #    Molecule Dissociation", Flinders University, 2000.
-# 
+#
 # Implemented in Python, with image quadrant co-adding, by Steve Gibson
 # 2015-12-16: Modified to calculate the forward Abel transform
-# 2015-12-03: Vectorization and code improvements Dan Hickstein and Roman Yurchak
+# 2015-12-03: Vectorization and code improvements Dan Hickstein and
+#             Roman Yurchak
 #             Previously the algorithm iterated over the rows of the image
 #             now all of the rows are calculated simultaneously, which provides
 #             the same result, but speeds up processing considerably.
@@ -32,31 +33,33 @@ import numpy as np
 
 def hansenlaw_transform(IM, dr=1, direction='inverse', **kwargs):
     r"""Forward/Inverse Abel transformation using the algorithm of
-    `Hansen and Law J. Opt. Soc. Am. A 2, 510-520 (1985) 
-    <http://dx.doi.org/10.1364/JOSAA.2.000510>`_ equation 2a: 
-    
-    
-    .. math:: f(r) = -\frac{1}{\pi} \int_{r}^{\infty} \frac{g^\prime(R)}{\sqrt{R^2-r^2}} dR,
-    
-    where 
+    `Hansen and Law J. Opt. Soc. Am. A 2, 510-520 (1985)
+    <http://dx.doi.org/10.1364/JOSAA.2.000510>`_ equation 2a:
+
+
+    .. math::
+
+     f(r) = -\frac{1}{\pi} \int_{r}^{\infty} \frac{g^\prime(R)}{\sqrt{R^2-r^2}} dR,
+
+    where
 
     :math:`f(r)` is the reconstructed image (source) function,
     :math:`g'(R)` is the derivative of the projection (measured) function
 
-    Evaluation follows Eqs. (15 or 17), using (16a), (16b), and (16c or 18) of 
-    the Hansen and Law paper. For the full image transform, use the 
+    Evaluation follows Eqs. (15 or 17), using (16a), (16b), and (16c or 18) of
+    the Hansen and Law paper. For the full image transform, use the
     class :class:``abel.Transform``.
 
     For the inverse Abel transform of image g: ::
-    
-        f = abel.Transform(g, direction="inverse", method="hansenlaw").transform
-        
+
+      f = abel.Transform(g, direction="inverse", method="hansenlaw").transform
+
     For the forward Abel transform of image f: ::
-    
-        g = abel.Transform(r, direction="forward", method="hansenlaw").transform
-        
-    This function performs the Hansen-Law transform on only one "right-side" image, 
-    typically one quadrant of the full image: ::
+
+      g = abel.Transform(r, direction="forward", method="hansenlaw").transform
+
+    This function performs the Hansen-Law transform on only one "right-side"
+    image, typically one quadrant of the full image: ::
 
         Qtrans = abel.hansenlaw.hansenlaw_transform(Q, direction="inverse")
 
@@ -82,12 +85,12 @@ def hansenlaw_transform(IM, dr=1, direction='inverse', **kwargs):
     -------
     AIM : 1D or 2D numpy array
         forward/inverse Abel transform half-image
-        
-        
-    .. note::  Image should be a right-side image, like this: ::  
-         
-        .         +--------      +--------+              
-        .         |      *       | *      |              
+
+
+    .. note::  Image should be a right-side image, like this: ::
+
+        .         +--------      +--------+
+        .         |      *       | *      |
         .         |   *          |    *   |  <---------- IM
         .         |  *           |     *  |
         .         +--------      o--------+
@@ -95,9 +98,9 @@ def hansenlaw_transform(IM, dr=1, direction='inverse', **kwargs):
         .         |   *          |    *   |
         .         |     *        | *      |
         .         +--------      +--------+
-          
+
         Image centre ``o`` should be within a pixel (i.e. an odd number of columns)
-        Use ``abel.tools.center.center_image(IM, center='com')`` 
+        Use ``abel.tools.center.center_image(IM, center='com')``
     """
 
     IM = np.atleast_2d(IM)
@@ -106,16 +109,15 @@ def hansenlaw_transform(IM, dr=1, direction='inverse', **kwargs):
 
     # Two alternative Gamma functions for forward/inverse transform
     # Eq. (16c) used for the forward transform
-    def fgamma(ratio, lam, n):
-        lam += 1
-        return 2*(n-1)*(1 - ratio**lam)/lam
-
     # Eq. (18) used for the inverse transform
     def igammalt(ratio, lam, nr):  # lam < 0
         return (1 - ratio**lam)/lam
 
     def igammagt(ratio, lam, nr):  # lam = 0
         return -np.log(ratio)
+
+    def fgamma(ratio, lam, n):
+        return 2*(n-1)*igammalt(ratio, lam+1, n)
 
     if direction == "inverse":   # inverse transform
         gammagt = igammagt   # special case lam = 0.0
@@ -124,18 +126,17 @@ def hansenlaw_transform(IM, dr=1, direction='inverse', **kwargs):
         # g' - derivative of the intensity profile
         gp = np.gradient(IM, axis=-1)
 
-    else:  # forward transform, common gamma function 
+    else:  # forward transform, common gamma function
         gammagt = gammalt = fgamma
         gp = IM
 
-    # phase shift -1/2 pixel to better align double transform with source
-    # and the agreement with analytical transform pairs (source and
+    # phase shift -1/2 pixel better aligns double transform with its source
+    # and better agreement with analytical transform pairs (source and
     # projections) see #206
     gp = (gp[:, 1:] + gp[:, :-1])/2
 
     # ------ The Hansen and Law algorithm ------------
     # iterate along columns, starting outer edge (right side)
-    # toward the image center
 
     # constants listed in Table 1.
     h = np.array([0.318, 0.19, 0.35, 0.82, 1.8, 3.9, 8.3, 19.6, 48.3])
@@ -151,7 +152,7 @@ def hansenlaw_transform(IM, dr=1, direction='inverse', **kwargs):
     ratio = ((cols - n)/(cols - n - 1))[::-1]  # R0/R
 
     # Gamma_n and Phi_n  Eq. (16a) and (16b), lam=0 special case (inverse)
-    Gamma[:, 0, 0] = h[0]*gammagt(ratio, lam[0], n)   
+    Gamma[:, 0, 0] = h[0]*gammagt(ratio, lam[0], n)
     Phi[:, 0, 0] = 1
 
     # lam < 0.0
