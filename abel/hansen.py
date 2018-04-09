163b2cb7c584e9f3e258a47e1057e9500c925b77
Stephen Gibson <Stephen.Gibson@anu.edu.au>
2018-04-09 13:53:11 +1000

    hansen 2D version

--- abel/hansen.py
+++ abel/hansen.py
@@ -1,68 +1,83 @@
-# -*- coding: utf-8 -*-
 import numpy as np
+import abel
 
+def hansen_transform(im, dr=1, direction='inverse', hold_order=1):
+    # Hansen IEEE Trans. Acoust. Speech Signal Proc. 33, 666 (1985)
+    #  10.1109/TASSP.1985.1164579
 
-def hansen_transform(IM, dr=1, direction='inverse', **kwargs):
-    def I(n, lam):
-        # integral (\epsilon/r)^\lamda d\epsilon
-        return 2*(n-1)*(1-(n/(n-1))**lam)/lam
-
-    # corrected beta expressions 10.1109/TASSP.1986.1164860
-    def beta0(n, lam):
-        # return 2*(n-1)*((n-1) + (l1-n+1)*ratio**l1)/l1/(l1+1)
-        return I(n, lam+1) - (n-1)*I(n, lam)
-
-    def beta1(n, lam):
-        # return -2*(n-1)*((l1+n) - n*ratio**l1)/l1/(l1+1)
-        return n*I(n, lam) - I(n, lam+1)
-
-    # parameters of Abel transform system model, table 1.
     h = np.array([0.318, 0.19, 0.35, 0.82, 1.8, 3.9, 8.3, 19.6, 48.3])
     lam = np.array([0.0, -2.1, -6.2, -22.4, -92.5, -414.5, -1889.4, -8990.9,
                     -47391.1])
 
-    IM = np.atleast_2d(IM)
+    # state equation integrals
+    def I(n, lam, a):  # integral (epsilon/r)^(lamda+pwr) 
+        K = lam.size
+        integral = np.empty((n.size, K))
+        lama = lam + a
+        for k in np.arange(K):
+            integral[:, k] = (1 - (n/(n-1))**lama[k])*(n-1)**a/lama[k]
 
-    AIM = np.zeros_like(IM)  # forward Abel transform image
+        return integral
 
-    rows, N = IM.shape  # shape of input quadrant (half)
-    # enumerate columns n = 0 is Rmax, the right side of image
-    n = np.arange(N-1, 0, -1)  # n = N-1, ..., 1
-    ratio = n[:-1]/n[1:]  # N-1/(N-2), ..., 2/1
+    # special case divide issue for lamda=0, only for inverse transform
+    def I0(n, lam, a):
+        K = lam.size
+        integral = np.empty((n.size, K))
+        integral[:, 0] = -np.log(n/(n-1))
+        for k in np.arange(1, K):
+             integral[:, k] = (1 - phi[:, k])/lam[k]
 
-    # phi array Eq (16a), diagonal array, for each pixel
-    K = h.size
-    phi = np.empty((N-2, K))
+        return integral
+
+    # first-order hold functions
+    def beta0(n, lam, a):  # fn   q\epsilon  +  p
+        return I(n, lam, a+1) - (n-1)*I(n, lam, a)
+
+    def beta1(n, lam, a):  # fn-1   p + q\epsilon
+        return n*I(n, lam, a) - I(n, lam, a+1)
+
+    im = np.atleast_2d(im)
+    aim = np.zeros_like(im)  # Abel transform array
+    rows, cols = im.shape
+    K = h.size 
+
+    N = np.arange(cols-1, 1, -1)  # N = cols-1, cols-2, ..., 2
+    ratio = N/(N-1)  # cols-1/cols-2, ...,  2/1 
+    
+    phi = np.empty((N.size, K))
     for k in range(K):
         phi[:, k] = ratio**lam[k]
 
-    B0 = np.zeros_like(phi)
-    B1 = np.zeros_like(phi)
-    for k in range(K):
-        B0[:, k] = h[k]*beta0(n[:-1], lam[k])
-        B1[:, k] = h[k]*beta1(n[:-1], lam[k])
+    if direction == 'forward':
+        drive = im.copy()
+        h *= -2*dr*np.pi  # include Jacobian with h-array
+        a = 1  # integration increases lambda + 1
+        integ = I
+    else:  # inverse Abel transform
+        drive = np.gradient(im, dr, axis=-1)
+        a = 0  # from 1/piR factor
+        integ = I0
 
-    # driving function = raw image. Copy so input image not mangled
-    if direction=='inverse':
-        drive = np.gradient(IM, axis=-1)
-    else:
-        drive = IM.copy()
-
-    # Hansen and Law Abel transform ---------------
     x = np.zeros((K, rows))
-    for indx, col in zip(n[::-1]-1, n[1:]):
-        x = phi[indx][:, None]*x + B0[indx][:, None]*drive[:, col+1]\
-                                 + B1[indx][:, None]*drive[:, col]
-        AIM[:, col] = x.sum(axis=0)
+
+    if hold_order:  # Hansen first-order hold approximation
+        B0 = beta0(N, lam, a, integ)
+        B1 = beta1(N, lam, a, integ)
+        for indx, col in zip(N[::-1]-N[-1], N):
+            x  = phi[indx][:, None]*x + (B0[indx]*h)[:, None]*drive[:, col]\
+                                      + (B1[indx]*h)[:, None]*drive[:, col-1]
+            aim[:, col-1] = x.sum()
+
+    else:  # Hansen zero-order hold approximation
+        gamma = integ(N, lam, a)
+        for indx, col in zip(N[::-1]-N[-1], N):
+            x  = phi[indx][:, None]*x + (gamma[indx]*h)[:, None]*drive[:, col-1]
+            aim[:, col-1] = x.sum()
 
     # missing 1st column
-    AIM[:, 0] = AIM[:, 1]
+    aim[:, 0] = aim[:, 1]
 
-    if AIM.shape[0] == 1:
-        AIM = AIM[0]   # flatten to a vector
+    if rows == 1:
+        aim = aim[0]  # flatter to a vector
 
-    print("here!")
-    if direction=='inverse':
-        return AIM*dr*2
-    else:
-        return AIM*dr*np.pi
+    return aim
