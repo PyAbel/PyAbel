6e1577ea15ba814fa85b1a4eab7d0d98b890bfff
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2018-09-16 00:08:36 -0600

    Updated function signatures in tests

--- abel/tests/test_basex.py
+++ abel/tests/test_basex.py
@@ -16,13 +16,13 @@ def test_basex_basis_sets_cache():
     # n_vert, n_horz = 121,121
     # nbf_vert, nbf_horz = 121, 61
     n = 121
-    file_name = os.path.join(DATA_DIR, "basex_basis_{}_{}_{}_{}.npy".format(n, n, n, n//2+1))
+    file_name = os.path.join(DATA_DIR, "basex_basis_{}_{}.npy".format(n, n//2+1))
     if os.path.exists(file_name):
         os.remove(file_name)
     # 1st call generate and save
-    abel.basex.get_bs_basex_cached(n,n, basis_dir=DATA_DIR, verbose=False)
+    abel.basex.get_bs_basex_cached(n, basis_dir=DATA_DIR, verbose=False)
     # 2nd call load from file
-    abel.basex.get_bs_basex_cached(n,n, basis_dir=DATA_DIR, verbose=False)
+    abel.basex.get_bs_basex_cached(n, basis_dir=DATA_DIR, verbose=False)
     if os.path.exists(file_name):
         os.remove(file_name)
 
@@ -30,18 +30,18 @@ def test_basex_basis_sets_cache():
 def test_basex_shape():
     n = 21
     x = np.ones((n, n), dtype='float32')
-    bs = abel.basex.get_bs_basex_cached(n,n, basis_dir=None, verbose=False)
+    Ai = abel.basex.get_bs_basex_cached(n, basis_dir=None, verbose=False)
 
-    recon = abel.basex.basex_core_transform(x, *bs)
+    recon = abel.basex.basex_core_transform(x, Ai)
 
     assert recon.shape == (n, n) 
 
 def test_basex_zeros():
     n = 21
     x = np.zeros((n, n), dtype='float32')
-    bs = abel.basex.get_bs_basex_cached(n,n, basis_dir=None, verbose=False)
+    Ai = abel.basex.get_bs_basex_cached(n, basis_dir=None, verbose=False)
 
-    recon = abel.basex.basex_core_transform(x, *bs)
+    recon = abel.basex.basex_core_transform(x, Ai)
 
     assert_allclose(recon, 0)
 
@@ -54,9 +54,9 @@ def test_basex_step_ratio():
     ref = abel.tools.analytical.GaussianAnalytical(n, r_max, symmetric=True,  sigma=10)
     tr = np.tile(ref.abel[None, :], (n, 1)) # make a 2D array from 1D
 
-    bs = abel.basex.get_bs_basex_cached(n,n, basis_dir=None, verbose=False)
+    Ai = abel.basex.get_bs_basex_cached(n, basis_dir=None, verbose=False)
 
-    recon = abel.basex.basex_core_transform(tr, *bs)
+    recon = abel.basex.basex_core_transform(tr, Ai)
     recon1d = recon[n//2 + n%2]
 
     ratio = abel.benchmark.absolute_ratio_benchmark(ref, recon1d)
