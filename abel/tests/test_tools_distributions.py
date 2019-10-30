d9d76e13ceeffd43d9227f99117f89e0cca4fbed
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2019-10-30 16:59:00 -0600

    Complete odd tests

--- abel/tests/test_tools_distributions.py
+++ abel/tests/test_tools_distributions.py
@@ -32,7 +32,7 @@ def run_order(method, tol):
     step = 6 * sigma
 
     for n in range(len(tol)):  # order = 2n
-        size = (n + 2) * step
+        size = int((n + 2) * step)
         # squared coordinates:
         x2 = np.arange(float(size))**2
         y2 = x2[:, None]
@@ -81,16 +81,17 @@ def run_order_odd(method, tol):
     step = 6 * sigma
 
     for n in range(0, len(tol)):  # order = n
-        size = (n + 2) * step
+        size = int((n + 2) * step)
         # coordinates:
         x = np.arange(float(size))
         y = size - np.arange(2 * float(size) + 1)[:, None]
         # radius
         r = np.sqrt(x**2 + y**2)
-        r[int(size), 0] = np.inf
+        # cos, sin
+        r[size, 0] = np.inf
         c = y / r
         s = x / r
-        r[0, 0] = 0
+        r[size, 0] = 0
 
         def peak(i):
             m = n - i
@@ -278,6 +279,179 @@ def test_remap():
     # (resampling of weights array in 'remap' makes "weq" differ)
 
 
+def run_method_odd(method, rmax, tolP0, tolP1, tolP2,
+                   tolI, tolbeta1, tolbeta2, weq=True):
+    """
+    method = method name
+    rmax = 'MIN' or 'all'
+    tol... = (atol, rmstol) for ...
+    tolbeta = atol for beta
+    weq = compare symbolic and array weights
+    """
+    # image size
+    n = 81  # width
+    m = 91  # height
+    # origin coordinates
+    xc = [0, 30, n // 2, 50, n - 1]
+    yc = [0, 25, m // 2, 65, m - 1]
+    # peak SD
+    sigma = 2.0
+
+    def peak(i, r):
+        return np.exp(-(r - i * step)**2 / (2 * sigma**2))
+
+    def image():
+        # coordinates:
+        x = np.arange(float(n)) - x0
+        y = y0 - np.arange(float(m))[:, None]
+        # radius:
+        r = np.sqrt(x**2 + y**2)
+        # cos, |sin|
+        r[y0, x0] = np.inf
+        c = y / r
+        s = np.abs(x) / r
+        s[y0, x0] = 1
+        r[y0, x0] = 0
+        # image: 3 peaks with different anisotropies
+        IM = s**2      * peak(1, r) + \
+             c**2      * peak(2, r) + \
+             (1/2 + c) * peak(3, r) + \
+                         peak(4, r)
+        return IM, s  # image, sin theta
+
+    def ref_distr():
+        r = np.arange(R + 1)
+        P0 = 2/3 * peak(1, r) + \
+             1/3 * peak(2, r) + \
+             1/2 * peak(3, r) + \
+                   peak(4, r)
+        P1 = peak(3, r)
+        P2 = -2/3 * peak(1, r) + \
+              2/3 * peak(2, r)
+        I = 4 * np.pi * r**2 * P0
+        beta1 = P1 / P0
+        beta2 = P2 / P0
+        return r, P0, P1, P2, I, beta1, beta2
+
+    for y0, x0 in itertools.product(yc, xc):
+        param = ' @ y0 = {}, x0 = {}, rmax = {}, method = {}'.\
+                format(y0, x0, rmax, method)
+
+        if rmax == 'MIN':
+            R = min(max(x0, n - 1 - x0), max(y0, m - 1 - y0))
+        elif rmax == 'MAX':
+            # exclude situations when the outer ring has insufficient vertical
+            # span for reliable even/odd separation
+            if y0 in [0, m - 1] and x0 not in [0, n - 1] or \
+               x0 == n // 2 and abs(y0 - m // 2) not in [0, m // 2]:
+                continue
+            R = max(max(x0, n - 1 - x0), max(y0, m - 1 - y0))
+        step = (R - 5 * sigma) / 4
+        refr, refP0, refP1, refP2, refI, refbeta1, refbeta2 = ref_distr()
+        f = 1 / (4 * np.pi * (1 + refr**2))  # rescaling factor for I
+
+        IM, ws = image()
+        w1 = np.ones_like(IM)
+
+        IMcopy = IM.copy()
+        w1copy = w1.copy()
+        wscopy = ws.copy()
+
+        weights = [(False, None, None),
+                   (True, None, None),
+                   (False, '1', w1),
+                   (False, 'sin', ws),
+                   (True, '1', w1)]
+        P0, P1, P2, r, I, beta1, beta2 = {}, {}, {}, {}, {}, {}, {}
+        for use_sin, wname, warray in weights:
+            weight_param = param + \
+                           ', sin = {}, weights = {}'.format(use_sin, wname)
+            key = (use_sin, wname)
+
+            distr = Distributions((y0, x0), rmax, odd=True, use_sin=use_sin,
+                                  weights=warray, method=method)
+            res = distr(IM)
+            P0[key], P1[key], P2[key] = res.harmonics()
+            r[key], I[key], beta1[key], beta2[key] = res.rIbeta()
+
+            def assert_cmp(msg, a, ref, tol):
+                atol, rmstol = tol
+                assert_allclose(a, ref, atol=atol,
+                                err_msg=msg + weight_param)
+                rms = np.sqrt(np.mean((a - ref)**2))
+                assert rms < rmstol, \
+                       '\n' + msg + weight_param + \
+                       '\nRMS error = {} > {}'.format(rms, rmstol)
+
+            assert_cmp('-> P0', P0[key], refP0, tolP0)
+            assert_cmp('-> P1', P1[key], refP1, tolP1)
+            assert_cmp('-> P2', P2[key], refP2, tolP2)
+
+            assert_equal(r[key], refr, err_msg='-> r' + weight_param)
+            assert_cmp('-> I', f * I[key], f * refI, tolI)
+            # beta values at peak centers
+            b1 = [round(beta1[key][int(i * step)], 5) for i in (1, 2, 3, 4)]
+            assert_allclose(b1, [0, 0, 2, 0], atol=tolbeta1,
+                            err_msg='-> beta1' + weight_param)
+            b2 = [round(beta2[key][int(i * step)], 5) for i in (1, 2, 3, 4)]
+            assert_allclose(b2, [-1, 2, 0, 0], atol=tolbeta2,
+                            err_msg='-> beta2' + weight_param)
+
+            assert_equal(IM, IMcopy,
+                         err_msg='-> IM corrupted' + weight_param)
+            assert_equal(w1, w1copy,
+                         err_msg='-> weights corrupted' + weight_param)
+            assert_equal(ws, wscopy,
+                         err_msg='-> weights corrupted' + weight_param)
+
+        if not weq:
+            continue
+        for key1, key2 in [((False, '1'), (False, None)),
+                           ((False, 'sin'), (True, None)),
+                           ((True, '1'), (False, 'sin'))]:
+            pair_param = param + ', sin + weights {} != {}'.format(key1, key2)
+            assert_allclose(P0[key1], P0[key2],
+                            err_msg='-> P0' + pair_param)
+            assert_allclose(P1[key1], P1[key2],
+                            err_msg='-> P1' + pair_param)
+            assert_allclose(P2[key1], P2[key2],
+                            err_msg='-> P2' + pair_param)
+            assert_allclose(I[key1], I[key2],
+                            err_msg='-> I' + pair_param)
+            assert_allclose(beta1[key1], beta1[key2],
+                            err_msg='-> beta1' + pair_param)
+            assert_allclose(beta2[key1], beta2[key2],
+                            err_msg='-> beta2' + pair_param)
+
+
+def test_nearest_odd():
+    run_method_odd('nearest', 'MIN',
+                   (0.093, 0.030), (0.21, 0.063), (0.17, 0.042),
+                   (0.093, 0.030), 0.18, 0.075)
+    run_method_odd('nearest', 'MAX',
+                   (0.16, 0.029), (0.27, 0.049), (0.12, 0.022),
+                   (0.16, 0.029), 0.080, 0.030)
+
+
+def test_linear_odd():
+    run_method_odd('linear', 'MIN',
+                   (0.025, 0.0078), (0.072, 0.028), (0.054, 0.019),
+                   (0.025, 0.0078), 0.17, 0.11)
+    run_method_odd('linear', 'MAX',
+                   (0.20, 0.040), (0.33, 0.064), (0.14, 0.027),
+                   (0.20, 0.040), 0.11, 0.086)
+
+
+def test_remap_odd():
+    run_method_odd('remap', 'MIN',
+                   (0.016, 0.0033), (0.029, 0.0060), (0.015, 0.0030),
+                   (0.016, 0.0033), 0.050, 0.012, weq=False)
+    run_method_odd('remap', 'MAX',
+                   (0.13, 0.025), (0.21, 0.041), (0.087, 0.017),
+                   (0.13, 0.025), 0.021, 0.011, weq=False)
+    # (resampling of weights array in 'remap' makes "weq" differ)
+
+
 def run_random(method, odd, use_sin, parts=True):
     """
     method = method name
@@ -382,14 +556,17 @@ if __name__ == '__main__':
     test_order_nearest()
     test_order_odd_nearest()
     test_nearest()
+    test_nearest_odd()
     test_nearest_random()
 
     test_order_linear()
     test_order_odd_linear()
     test_linear()
+    test_linear_odd()
     test_linear_random()
 
     test_order_remap()
     test_order_odd_remap()
     test_remap()
+    test_remap_odd()
     test_remap_random()
