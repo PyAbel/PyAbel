bc98d7b95c5dea06fbbd00a653af223ea193ca8e
DanHickstein <danhickstein@gmail.com>
2020-04-29 00:36:17 -0600

    somewhat fewer pep8 problems

--- doc/transform_methods/comparison/Fig_experiment/experiment.py
+++ doc/transform_methods/comparison/Fig_experiment/experiment.py
@@ -1,21 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys, os
 import abel
-import scipy.ndimage
 import bz2
 
 transforms = [
-  ("basex"        , abel.basex.basex_transform              , '#880000'),
-  ("direct"       , abel.direct.direct_transform            , '#EE0000'),
-  ("hansenlaw"    , abel.hansenlaw.hansenlaw_transform      , '#CCAA00'),
-  ("onion_bordas" , abel.onion_bordas.onion_bordas_transform, '#00AA00'),
-  ("onion_peeling", abel.dasch.onion_peeling_transform      , '#00CCFF'),
-  ("three_point"  , abel.dasch.three_point_transform        , '#0000FF'),
-  ("two_point"    , abel.dasch.two_point_transform          , '#CC00FF'),
-  ("linbasex"     , abel.linbasex.linbasex_transform        , '#a0a0a0'),
-  ("rbasex",        abel.rbasex.rbasex_transform            , '#00AA00'),
-  
+  ("basex",         abel.basex.basex_transform,               '#880000'),
+  ("direct",        abel.direct.direct_transform,             '#EE0000'),
+  ("hansenlaw",     abel.hansenlaw.hansenlaw_transform,       '#CCAA00'),
+  ("onion_bordas",  abel.onion_bordas.onion_bordas_transform, '#00AA00'),
+  ("onion_peeling", abel.dasch.onion_peeling_transform,       '#00CCFF'),
+  ("three_point",   abel.dasch.three_point_transformm,        '#0000FF'),
+  ("two_point",     abel.dasch.two_point_transform,           '#CC00FF'),
+  ("linbasex",      abel.linbasex.linbasex_transform,         '#a0a0a0'),
+  ("rbasex",        abel.rbasex.rbasex_transform,             '#00AA00'),
 ]
 
 
@@ -24,16 +21,19 @@ ntrans = len(transforms)  # number of transforms
 infile = bz2.BZ2File('../../../../examples/data/O2-ANU1024.txt.bz2')
 IM = np.loadtxt(infile)
 
-IModd = abel.tools.center.center_image(IM, origin="convolution", odd_size=True, square=True)
+IModd = abel.tools.center.center_image(IM, origin="convolution",
+                                       odd_size=True, square=True)
+
 Q = abel.tools.symmetry.get_image_quadrants(IModd, reorient=True)
 Q0 = Q[1]
 
 h, w = np.shape(IM)
 
-fig, axs  = plt.subplots(3, 4, figsize=(8, 6), sharex=True, sharey=True)
-fig1,axs1 = plt.subplots(3, 1, figsize=(3.37, 5))
+fig, axs = plt.subplots(3, 4, figsize=(8, 6), sharex=True, sharey=True)
+fig1, axs1 = plt.subplots(3, 1, figsize=(3.37, 5))
 
-for num, (ax, (label, transFunc, color)) in enumerate(zip(axs.ravel(), transforms)):
+for num, (ax, (label, transFunc, color)) in enumerate(zip(axs.ravel(),
+                                                          transforms)):
     print(label)
 
     if label == 'linbasex':
@@ -43,25 +43,25 @@ for num, (ax, (label, transFunc, color)) in enumerate(zip(axs.ravel(), transform
 
     if label == 'basex':
         targs = dict(reg=200)
-    
+
     if label == 'rbasex':
         trans = transFunc(IModd, direction="inverse", order=2)[0]
-        trans = abel.tools.symmetry.get_image_quadrants(trans, reorient=True)[1]
-        # trans = trans/2e2
+        trans = abel.tools.symmetry.get_image_quadrants(trans,
+                                                        reorient=True)[1]
     else:
         trans = transFunc(Q0, direction="inverse", **targs)
     if label == 'linbasex':  # bugfix smoothing=0 transform offset by 1 pixel
         trans[:, 1:] = trans[:, :-1]
 
-
-    r, inten = abel.tools.vmi.angular_integration(trans[::-1], origin=(0, 0), dr=0.1)
+    r, inten = abel.tools.vmi.angular_integration(trans[::-1],
+                                                  origin=(0, 0),
+                                                  dr=0.1)
 
     inten /= 1e6
-    
-    im = ax.imshow(trans[::-1], cmap='gist_heat_r',
-              origin='lower', aspect='auto', vmin=0,
-              vmax=5)
-    
+
+    im = ax.imshow(trans[::-1], cmap='gist_heat_r', origin='lower',
+                   aspect='auto', vmin=0, vmax=5)
+
     ax.set_title(label, fontsize=10, x=0.96, y=0.90, ha='right', va='top',
                  weight='bold', color='k')
 
@@ -93,21 +93,21 @@ for ax in axs.ravel():
     ax.set_yticklabels(major, fontdict={'fontsize': 6},
                        rotation='vertical', verticalalignment='center')
     ax.set_yticks(minor, minor=True)
-    
+
 fig.subplots_adjust(left=0.13, bottom=0.06, right=0.99, top=0.91, wspace=0.08,
                     hspace=0.08)
 
-axs[-1,0].set_xlabel('$r$ (pixels)')
-axs[-1,1].set_xlabel('$r$ (pixels)')
+axs[-1, 0].set_xlabel('$r$ (pixels)')
+axs[-1, 1].set_xlabel('$r$ (pixels)')
 
-for ax in axs[:,0]:
+for ax in axs[:, 0]:
     ax.set_ylabel('$z$ (pixels)')
 
 
 for ax in axs1:
     for label in ax.get_xticklabels() + ax.get_yticklabels():
         label.set_fontsize(8)
-    
+
     ax.grid(color='k', alpha=0.1)
 
 axs1[0].set_xlim(0, 512)
@@ -121,11 +121,13 @@ axs1[2].set_xlim(80, 160)
 axs1[2].set_xticks(np.arange(80, 160, 10), minor=True)
 axs1[2].set_ylim(-0.004, 0.02)
 
-def place_letter(letter, ax, color='k', offset=(0,0)):
+
+def place_letter(letter, ax, color='k', offset=(0, 0)):
     ax.annotate(letter, xy=(0.02, 0.97), xytext=offset,
                 xycoords='axes fraction', textcoords='offset points',
                 color=color, ha='left', va='top', weight='bold')
-    
+
+
 for ax, letter in zip(axs.ravel(), 'abcdefgh'):
     place_letter(letter+')', ax)
 
