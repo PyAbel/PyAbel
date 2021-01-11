b4e154050fa0503ce74b2258555d90ddb6114ae4
DanHickstein <danhickstein@gmail.com>
2021-01-10 21:23:10 -0700

    included py38 and 39 in setup.py

--- setup.py
+++ setup.py
@@ -132,6 +132,8 @@ setup(name='PyAbel',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           ],
       **setup_args
       )
