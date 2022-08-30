5c6da761d9e77c8e65bb46964351d90aeaed011e
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2022-08-30 18:02:36 +0300

    Same normalization for all methods (for fair comparison)

--- examples/example_all_O2.py
+++ examples/example_all_O2.py
@@ -53,7 +53,7 @@ Q0 = Q[0]
 Q0fresh = Q0.copy()    # keep clean copy
 print("quadrant shape {}".format(Q0.shape))
 
-# Intensity mask used for intensity normalization
+# Intensity mask used for intensity scale
 #   quadrant image region of bright pixels
 mask = np.zeros(Q0.shape, dtype=bool)
 mask[500:512, 358:365] = True
@@ -61,7 +61,9 @@ mask[500:512, 358:365] = True
 # process Q0 quadrant using each method --------------------
 
 iabelQ = []  # keep inverse Abel transformed image
+vmax = 0  # intensity scale
 sp = []  # speed distributions
+ymax = 0  # normalization
 meth = []  # methods
 
 for q, method in enumerate(sorted(transforms.keys())):
@@ -72,16 +74,16 @@ for q, method in enumerate(sorted(transforms.keys())):
     t0 = time()
 
     # inverse Abel transform using 'method'
-    IAQ0 = transforms[method](Q0, direction="inverse", dr=0.1)
+    IAQ0 = transforms[method](Q0, direction="inverse")
     print("                    {:.1f} s".format(time()-t0))
 
     # polar projection and speed profile
     radial, speed = abel.tools.vmi.angular_integration_3D(IAQ0, origin=(-1, 0),
                                                           dr=0.1)
 
-    # normalize image intensity and speed distribution
-    IAQ0 /= IAQ0[mask].max()
-    speed /= speed[radial > 50].max()
+    # update normalization
+    vmax = max(vmax, IAQ0[mask].max())
+    ymax = max(ymax, speed[radial > 50].max())
 
     # keep data for plots
     iabelQ.append(IAQ0)
@@ -91,15 +93,16 @@ for q, method in enumerate(sorted(transforms.keys())):
 # reassemble image, each quadrant a different method
 
 # plot inverse Abel transformed image slices, and respective speed distributions
+plt.figure(figsize=(12, 6))
 ax0 = plt.subplot2grid((1, 2), (0, 0))
 ax1 = plt.subplot2grid((1, 2), (0, 1))
 
 def ann_plt(quad, subquad, txt):
     # -ve because numpy coords from top
-    annot_angle = -(30+30*subquad+quad*90)*np.pi/180
+    annot_angle = -(22.5+45*subquad+quad*90)*np.pi/180
     annot_coord = (h/2+(h*0.8)*np.cos(annot_angle)/2,
                    w/2+(w*0.8)*np.sin(annot_angle)/2)
-    ax0.annotate(txt, annot_coord, color="yellow", ha='center')
+    ax0.annotate(txt, annot_coord, color="white", ha='center')
 
 # for < 4 images pad using a blank quadrant
 r, c = Q0.shape
@@ -110,12 +113,12 @@ iq = 0
 for q in range(4):
     Q[q] = iabelQ[iq].copy()
     ann_plt(q, 0, meth[iq])
-    ax1.plot(*(sp[iq]), label=meth[iq], alpha=0.5)
+    ax1.plot(sp[iq][0], sp[iq][1] / ymax, label=meth[iq], alpha=0.5)
     iq += 1
     if iq < len(transforms):
         (Q[q][::-1])[indx] = (iabelQ[iq][::-1])[indx]
         ann_plt(q, 1, meth[iq])
-        ax1.plot(*(sp[iq]), label=meth[iq], alpha=0.5)
+        ax1.plot(sp[iq][0], sp[iq][1] / ymax, label=meth[iq], alpha=0.5)
     iq += 1
 
 # reassemble image from transformed (part-)quadrants
@@ -124,10 +127,10 @@ im = abel.tools.symmetry.put_image_quadrants((Q[0], Q[1], Q[2], Q[3]),
 
 ax0.axis('off')
 ax0.set_title("inverse Abel transforms")
-ax0.imshow(im, vmin=0, vmax=0.8)
+ax0.imshow(im, vmin=0, vmax=0.8*vmax, interpolation='nearest')
 
 ax1.set_title("speed distribution")
-ax1.axis(ymin=-0.05, ymax=1.1, xmin=50, xmax=450)
+ax1.axis(xmin=50, xmax=450)
 ax1.legend(loc=0, labelspacing=0.1, fontsize=10, frameon=False)
 plt.tight_layout()
 
