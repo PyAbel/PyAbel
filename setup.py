c50c7c3aa6e6676ae379df821f1aefa6f3fe37dd
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2022-09-09 18:26:13 +0300

    Add Python 3.10 test

--- setup.py
+++ setup.py
@@ -130,10 +130,10 @@ setup(name='PyAbel',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           ],
       **setup_args
       )
